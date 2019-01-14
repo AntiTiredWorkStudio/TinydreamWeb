1a924122f86cc83e96e021c5028006535a0f41ae
whugobarry <17600740311@163.com>
2019-01-15 02:47:21 +0800

    测试

--- js/payInfo.js
+++ js/payInfo.js
@@ -1,28 +1,17 @@
 $(function(){
-    var userInfo = Options.GetUserInfo();
-    TD_Request("us", "enter",
-      {
-        uid: userInfo.openid,
-        nickname: userInfo.nickname,
-        headicon: userInfo.headicon,
-      }, function (code, data) {
-        //请求成功的处理
-      }, function (code, data) {
-       //请求失败的处理
-      }
-    );
-    // var canvas = document.getElementById('canvas');
-    // var ctx = canvas.getContext("2d");
-    // function ready(){
-    //     var canvas = document.getElementById('bottom');
-    //     var cxt_arc = canvas.getContext("2d");
-    //     cxt_arc.lineWidth = 15;
-    //     cxt_arc.strokeStyle = '#edf0f5';
-    //     cxt_arc.lineCap = 'round';
-    //     cxt_arc.beginPath();
-    //     cxt_arc.arc(95, 95, 80, 0, 2 * Math.PI, false);
-    //     cxt_arc.stroke();
-    // }
+    ready();
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext("2d");
+    function ready(){
+        var canvas = document.getElementById('bottom');
+        var cxt_arc = canvas.getContext("2d");
+        cxt_arc.lineWidth = 15;
+        cxt_arc.strokeStyle = '#edf0f5';
+        cxt_arc.lineCap = 'round';
+        cxt_arc.beginPath();
+        cxt_arc.arc(95, 95, 80, 0, 2 * Math.PI, false);
+        cxt_arc.stroke();
+    }
     // function drawCircle(ctx,prop){
     //     if(prop == 0){
     //         ctx.clearRect(0,0,190,190);
@@ -39,4 +28,4 @@ $(function(){
     //     ctx.arc(95, 95, 80, Math.PI/-2, prop * Math.PI - Math.PI / 2, false);
     //     ctx.stroke()
     // }
-})
+});
