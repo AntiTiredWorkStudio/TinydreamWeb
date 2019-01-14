824fa948cb7b99c64d300ca9fe53350383c19427
whugobarry <17600740311@163.com>
2019-01-14 14:20:06 +0800

    测试

--- js/index.js
+++ js/index.js
@@ -1,10 +1,9 @@
 $(function(){
      // 检测是否登录
-    WebApp.Init('wxc5216d15dd321ac5',
-        function(result,data){
-            console.log(Options.GetUserInfo());
-            $('#test').html(JSON.stringify(Options.GetUserInfo())); 
-            // console.log(userInfo)
+     WebApp.Init('wxdca9f5edc034c1a0',//appid
+        function(result,data){//result:请求状态,data 请求结果
+           console.log(Options.GetUserInfo());
+           $('#test').html(JSON.stringify(Options.GetUserInfo()));
         }
     );
     var canvas = document.getElementById('canvas');
