bda2f009958e92e0cc577c4cc6c200d2fd39cfde
whugobarry <17600740311@163.com>
2019-01-15 03:08:41 +0800

    测试

--- js/payInfo.js
+++ js/payInfo.js
@@ -1,4 +1,6 @@
 $(function(){
+    var buy = localStorage.getItem('buy');
+    console.log(buy);
     ready();
     var canvas = document.getElementById('canvas');
     var ctx = canvas.getContext("2d");
