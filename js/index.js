c06d475de0b31d1d05c2bdeeb5d480809ed6c09f
whugobarry <17600740311@163.com>
2019-01-14 17:59:32 +0800

    测试

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ $(function(){
      // 检测是否登录
      WebApp.Init('wxc5216d15dd321ac5',//appid
         function(result,data){//result:请求状态,data 请求结果
+           console.log(result,data);
            var userInfo = Options.GetUserInfo();
            console.log(userInfo.openid)
            $('#test').html(JSON.stringify(Options.GetUserInfo()));
