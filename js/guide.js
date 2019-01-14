01dd8fe1559aa9b53a7904be45fe93116a06bab5
whugobarry <17600740311@163.com>
2019-01-14 11:15:39 +0800

    测试提交

--- js/guide.js
+++
@@ -1,6 +0,0 @@
-WebApp.Init('wxdca9f5edc034c1a0',//appid
-   function(result,data){//result:请求状态,data 请求结果
-      console.log(Options.GetUserInfo());
-    //   $('#test').html(JSON.stringify(Options.GetUserInfo()));
-   }
-);
