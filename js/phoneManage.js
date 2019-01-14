a744a0696394b8be581a83c9a1c6cff69c6a499a
whugobarry <17600740311@163.com>
2019-01-15 00:27:18 +0800

    测试

--- js/phoneManage.js
+++ js/phoneManage.js
@@ -22,7 +22,7 @@ $(function(){
     })
     // 验证手机号规则
     var reg = /^(13[0-9]|14[579]|15[0-3,5-9]|16[6]|17[0135678]|18[0-9]|19[89])\d{8}$/;
-    $('.phoneNum input').oninput(function(){
+    $('.phoneNum input').on('input',function(){
         console.log($(this).val());
     })
     // 获取手机验证码
