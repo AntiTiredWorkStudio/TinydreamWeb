f54ad41a7b4051eb6b5fde6f7da3c02f31bbd262
whugobarry <17600740311@163.com>
2019-01-15 00:21:45 +0800

    测试

--- js/phoneManage.js
+++ js/phoneManage.js
@@ -5,9 +5,25 @@ $(function(){
         uid:userInfo.openid,
     },function(code,data){
         // 请求成功
-        console.log(code,data)
+        if(code == 0){
+            if(data.tele == ""){
+                $('.oldPhoneNum').hide();
+            }else{
+                $('.oldPhoneNum').html(data.tele);
+                $('.oldPhoneNum').show();
+            }
+        }
     },function(code,data){
         // 请求失败
-        console.log(code,data);
+        if(code!=0){
+            alert('参数错误'+data.context);
+            return;
+        }
     })
+    // 验证手机号规则
+    var reg = /^(13[0-9]|14[579]|15[0-3,5-9]|16[6]|17[0135678]|18[0-9]|19[89])\d{8}$/;
+    $('.phoneNum input').input(function(){
+        console.log($(this).val());
+    })
+    // 获取手机验证码
 })
