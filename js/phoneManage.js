1b374f86ac8cae78dd7c245b1ce3b76b3b3de116
whugobarry <17600740311@163.com>
2019-01-15 00:12:41 +0800

    测试

--- js/phoneManage.js
+++ js/phoneManage.js
@@ -1,5 +1,13 @@
 $(function(){
     // 获取用户信息
     var userInfo = Options.GetUserInfo()
-    console.log(userInfo);
+    TD_Request("va", "pbind", {
+        uid:userInfo.openid,
+    },function(code,data){
+        // 请求成功
+        console.log(code,data)
+    },function(code,data){
+        // 请求失败
+        console.log(code,data);
+    })
 })
