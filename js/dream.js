d4a0658bdd3d98543db47df6c8ad8f5e968e84c9
whugobarry <17600740311@163.com>
2019-01-15 05:56:38 +0800

    测试

--- js/dream.js
+++ js/dream.js
@@ -1,4 +1,15 @@
 $(function(){
+    // 获取梦想列标
+    var userInfo = Options.GetUserInfo();
+    TD_Request("dr","dlist",{
+        uid:userInfo.openid
+    },function(code,data){
+        if(code == 1){
+            console.log(data)
+        }
+    },function(code,data){
+        console.log(data);
+    })
     // 查看小梦想详情
     $('.view').click(function(){
         window.location.href="../html/add.html";
