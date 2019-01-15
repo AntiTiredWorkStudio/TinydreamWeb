ad3fda3e8d1619450e10e8a1715745908a284191
whugobarry <17600740311@163.com>
2019-01-15 08:15:03 +0800

    测试

--- js/dream.js
+++ js/dream.js
@@ -30,7 +30,7 @@ $(function(){
         }else if($('.info').val() == ""){
             alert("梦想简介不能为空");
             return;
-        }else if($('.check').val()!="on"){
+        }else if(!$("input[type='checkbox']").is(':checked')){
             alert('您还没有同意协议呢！');
             return;
         }else{
