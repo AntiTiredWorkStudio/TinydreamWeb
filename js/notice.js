97994d7d1e57c7d2d3f51ce76a71318ad983e921
whugobarry <17600740311@163.com>
2019-01-16 03:40:28 +0800

    测试

--- js/notice.js
+++ js/notice.js
@@ -1,3 +1,13 @@
 $(function(){
-    
+    var userInfo = Options.GetUserInfo();
+
+    TD_Request("no","ng",{
+        uid:userInfo.openid,
+        seek:0,
+        count:20
+    },function(code,data){
+        console.log(data)
+    },function(code,data){
+        console.log(data)
+    })
 })
