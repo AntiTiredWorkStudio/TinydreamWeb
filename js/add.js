8215c565a6b80323da714300457de34ded3487fb
whugobarry <17600740311@163.com>
2019-01-16 22:13:13 +0800

    测试

--- js/add.js
+++ js/add.js
@@ -2,7 +2,7 @@ $(function(){
     var userInfo = Options.GetUserInfo();
     var dr = JSON.parse(localStorage.getItem('dr'));
     // 获取梦想id
-    get(userInfo,dr)
+    get(userInfo,dr,state = dr.state)
     if(dr.state!="all"){
         var state = "all"
         $('.supper').show();
