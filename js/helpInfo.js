b65d81bc26708cff6ed7787c6864189573f7654e
whugobarry <17600740311@163.com>
2019-01-17 04:22:05 +0800

    测试

--- js/helpInfo.js
+++ js/helpInfo.js
@@ -14,7 +14,7 @@ $(function(){
         $('.join_help').removeAttr('disabled').html('参与互助').css({background:'#00d094',color:'#fff'})
     }else if(poolInfo.state != "RUNNING" && poolInfo.award == "NO"){
         $('.state').html('已结束');
-        $('.join_help').attr('disabled').html('等待开奖').css({background:'#ccc',color:'#b2b2b2'})
+        $('.join_help').attr('disabled').val('等待开奖').css({background:'#ccc',color:'#b2b2b2'})
     }else if(poolInfo.state != "RUNNING" && poolInfo.award == "YES"){
         $('.state').html('已结束');
         $('.join_help').attr('disabled').html('已完成').css({background:'#f60',color:'#fff'})
