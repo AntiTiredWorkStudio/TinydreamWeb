d7364d1c08b2a7e9d363f4680c455349a8b8aad2
whugobarry <17600740311@163.com>
2019-01-17 04:18:04 +0800

    测试

--- js/helpInfo.js
+++ js/helpInfo.js
@@ -9,7 +9,7 @@ $(function(){
     $('.help_money').html(poolInfo.realBill + poolInfo.realUnit)
     $('.present_money').html(poolInfo.billHint);
     $('.target_money').html(poolInfo.rtbillValue+poolInfo.rtbillUnit);
-    if(poolInfo.state == "RUNNING") {
+    if(poolInfo.state == "RUNNING" && poolInfo.award == "NO") {
         $('.state').html('互助中');
         $('.join_help').removeAttr('disabled').html('参与互助').css({background:'#00d094',color:'#fff'})
     }else if(poolInfo.state != "RUNNING" && poolInfo.award == "NO"){
@@ -24,8 +24,53 @@ $(function(){
     TD_Request('aw','lfromp',{
         pid:poolInfo.pid,
     },function(code,data){
-        console.log(data);
+        if(poolInfo.state != "RUNNING" && code == 0){
+            var award = code;
+            TD_Request('ds','pdetial',{
+                uid:userInfo.openid,
+                pid:poolInfo.pid
+            },function(code,data){
+                console.log(data)
+                $.each(data.lottey,function(key,value){
+                    console.log(value);
+                    $('.state').html(value.lid);
+                })
+            },function(code,data){
+                console.log("获取失败："+data.context)
+            })
+            $('.join_help').attr('disabled').html('已完成').css({background:'#f60',color:'#fff'})
+        }
+    },function(code,data){
+        console.log('获取失败：'+data.context)
+        if(poolInfo.state == "RUNNING" && code == 59) {
+            $('.state').html('互助中');
+            $('.join_help').removeAttr('disabled').html('参与互助').css({background:'#00d094',color:'#fff'})
+        }else if(poolInfo.state != "RUNNING" && code == 59){
+            $('.state').html('已结束');
+            $('.join_help').attr('disabled').html('等待开奖').css({background:'#ccc',color:'#b2b2b2'})
+        }
+    })
+
+    // 获取购买总数
+    TD_Request('ds','precs',{
+        pid:poolInfo.pid,
+    },function(code,data){
+        $('.count').html(data.ordCount)
     },function(code,data){
         console.log('获取失败：'+data.context)
     })
+
+    // 编号类型切换
+    var number = 0; //初始状态
+    $('.tab div').click(function(){
+        $(this).addClass('active').siblings().removeClass('active')
+    })
+    $('.tab div.left').click(function(){
+       
+    })
+
+    // 公用
+    function getOrd(number){
+        console.log(userInfo,poolInfo)
+    }
 })
