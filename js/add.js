94b64c67e75bddf7422b0e124eddad6ab0d2a4a0
whugobarry <17600740311@163.com>
2019-01-16 19:11:33 +0800

    测试

--- js/add.js
+++ js/add.js
@@ -16,9 +16,31 @@ $(function(){
         state: state
     },function(code,data){
         console.log(data)
-        $('.title').val(data.dream.title);
+        $('.dr_title').val(data.dream.title);
         $('.dr_info').val(data.dream.content);
     },function(code,data){
         console.log("错误信息："+data.context);
+    })
+    $('.submit').click(function(){
+        if($('.dr_title').val() == ''){
+            alert('梦想标题不能为空');
+            return;
+        }else if($('.dr_info').val() == ''){
+            alert('梦想简介不能为空');
+            return;
+        }else if(!$('input[type="checkbox"]:checked')){
+            alert('请勾选协议');
+            return;  
+        }else{
+            TD_Request('dr','dedit',{
+                uid:userInfo.openid,
+                title:$('.dr_title').val(),
+                content:$('.dr_info').val()
+            },function(code,data){
+                console.log(data)
+            },function(code,data){
+                console.log(data)
+            })
+        }
     })
 })
