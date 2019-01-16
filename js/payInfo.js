bcb85a7bb7c3194055d53e0ac7da72a914759bd9
whugobarry <17600740311@163.com>
2019-01-16 14:41:47 +0800

    测试

--- js/payInfo.js
+++ js/payInfo.js
@@ -100,7 +100,7 @@ $(function(){
                     $('.mask').fadeIn();
                     var number = data.numbers;
                     for(key in number){
-                      alert(number[key])
+                      alert(JSON.stringify(number[key]))
                     }
                     alert(JSON.stringify(data.numbers))
                   },function(code,data){
@@ -132,7 +132,7 @@ $(function(){
           console.log(arr)
         })
         $('.dream').select({
-          title: "选择梦想",
+          // title: "选择梦想",
           items: arr,
           onClose:function(){
             $('.dream strong').html($('.weui_cell_ft input:radio[name="weui-select"]:checked').parent('.weui_cell_ft').prev().children('p').html())
