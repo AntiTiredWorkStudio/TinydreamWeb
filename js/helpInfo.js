864b8bd881bd1c895e9c7f5727c0f203d97dea39
whugobarry <17600740311@163.com>
2019-01-17 19:46:39 +0800

    测试

--- js/helpInfo.js
+++ js/helpInfo.js
@@ -42,6 +42,32 @@ $(function(){
         $('.state').html('互助中');
         $('.join_help').html('参与互助').css({background:'#00d094',color:'#fff'}).removeAttr('disabled')
         $('.join_help').click(function(){
+            TD_Request('ds','buy',{
+                uid:userInfo.openid,
+                pid:poolInfo.pid
+            },function(code,data){
+                if(code == 0 || data.result == true){
+                    console.log(data)
+                    if(data.actions.buy.dream == "" || data.actions.buy.dream == undefined){
+                        if(confirm("您还没有添加梦想，添加梦想后才能参与互助")){
+                            window.location.href = "http://tinydream.antit.top/TinydreamWeb/html/dream.html"
+                        }else{
+                            window.location.href = "http://tinydream.antit.top/TinydreamWeb/index.html"
+                        }
+                    }
+                    localStorage.setItem('buy',JSON.stringify(data.actions));
+                    window.location.href = "http://tinydream.antit.top/TinydreamWeb/html/payInfo.html";
+                }
+            },function(code,data){
+                console.log(data)
+                if(code == 11 || !data.result){
+                    alert("您尚"+data.context+",绑定手机后才能继续参与互助");
+                    window.location.href = "http://tinydream.antit.top/TinydreamWeb/html/phoneManage.html";
+                }else if(code == 18){
+                    alert("您当日购买次数已达上限");
+                    return;
+                }
+            })
             window.location.href = "http://tinydream.antit.top/TinydreamWeb/html/payInfo.html"
         })
     }else if(poolInfo.state != "RUNNING"){
