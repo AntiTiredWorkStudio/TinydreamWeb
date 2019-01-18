d1b037c5b26f50fb5435e68fe0e37c2aa8ed9968
whugobarry <17600740311@163.com>
2019-01-18 16:22:12 +0800

    测试

--- js/index.js
+++ js/index.js
@@ -89,8 +89,6 @@ $(function(){
                             window.location.href = "http://tinydream.antit.top/TinydreamWeb/html/payInfo.html";
                         }
                     },function(code,data){
-                        // console.log(data);
-                        return;
                         if(code == 11 || !data.result){
                             alert("您尚"+data.context+",绑定手机后才能继续参与互助");
                             localStorage.setItem('mainpool',JSON.stringify(mainpool));
