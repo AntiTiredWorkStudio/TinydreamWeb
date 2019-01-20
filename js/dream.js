6bc366a6dcbe1e1e525bd27873430d71b9981868
whugobarry <17600740311@163.com>
2019-01-20 16:04:00 +0800

    复选框修改

--- js/dream.js
+++ js/dream.js
@@ -112,6 +112,10 @@ $(function(){
             })
         }
     })
+    $("input[type='checkbox']:checked").parent('label').css({
+        background:"url(https://tdream.antit.top/image/Toggle_True.jpg) no-repeat",
+        "background-size":"0.3rem 0.3rem"
+    })
     $('.xieyi').click(function(){
         window.location.href = "http://tinydream.antit.top/TinydreamWeb/html/xieyi.html"
     })
