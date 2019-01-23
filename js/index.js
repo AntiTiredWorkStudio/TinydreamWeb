02b43c040630f341de9250c353aa6b2588836b44
whugobarry <17600740311@163.com>
2019-01-23 13:45:04 +0800

    提交

--- js/index.js
+++ js/index.js
@@ -48,8 +48,11 @@ $(function(){
                         $('.tip').hide();
                         $('.logo').css('background-image','url('+info.headicon+')');
                         $('.username').html(info.nickname);
+                        var date = parseInt(new Date().getTime() / 1000);
+                        var time = DescriptionTime(date - item.ptime);
+                        info.time = time;
                         $('.male_tip').html(info.time+"前参与了"+info.dcount+"份小梦想")
-                    },1000)
+                    },2000)
                     console.log(info)
                    
                     // var templateStr = $('#template').html();
