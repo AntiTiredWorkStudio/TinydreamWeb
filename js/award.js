cc9a9defdbf34dfcf491ea01b3955ef27c6d0a6f
whugobarry <17600740311@163.com>
2019-01-25 03:16:36 +0800

    提交

--- js/award.js
+++ js/award.js
@@ -11,9 +11,12 @@ $(function(){
             $('.title .left').html('完善梦想')
             $('.title .right').html('等待完善')
             $('.tip').html('小梦想 —— 幸运梦想 —— 完善梦想即可');
-            $('.progress_main .index').addClass('active')
+            $('.progress_main .index').eq(1).addClass('active')
         }else if(data.dream.state == 'VERIFY'){
-            
+            $('.title .left').html('资料审核')
+            $('.title .right').html('审核中').css('color','green')
+            $('.tip').html('资料审核1——2个工作日');
+            $('.progress_main .index.active line').addClass('activeLine')
         }
     },function(code,data){
         console.log(data)
