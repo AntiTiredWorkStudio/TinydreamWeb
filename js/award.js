d786953216f0211379e547303110534d56ccdfaa
whugobarry <17600740311@163.com>
2019-01-25 03:06:39 +0800

    提交

--- js/award.js
+++ js/award.js
@@ -7,6 +7,12 @@ $(function(){
         state:'all'
     },function(code,data){
         console.log(data)
+        if(data.dream.state == 'DOING'){
+            $('.title .left').html('完善梦想')
+            $('.title .right').html('等待完善')
+            $('.tip').html('小梦想 —— 幸运梦想 —— 完善梦想即可');
+            $('.progress_main .index').addClass('active')
+        }else if(data.dream.state == '')
     },function(code,data){
         console.log(data)
     })
