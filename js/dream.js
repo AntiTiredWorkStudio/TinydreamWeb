800d55ecd4e464029106ad90ba5bf26a367b28be
whugobarry <17600740311@163.com>
2019-01-24 18:17:15 +0800

    提交

--- js/dream.js
+++ js/dream.js
@@ -66,10 +66,13 @@ $(function(){
                     }else{
                         _.each(data.dreams,function(item){
                             if(item.state == "SUCCESS" || item.state == "DOING"){
-                                $("<div class='luckyDream'><div class='dream_logo'>"+item.pool.tbill / 100+"</div><div class='dream_right'><div class='dream_msg'><span>"+item.title+"</span><div class='icon_success'></div></div> <div class='tip'><span class='tip_text'>"+item.pool.ptitle+"</span></div></div></div>").appendTo('..dream_list')
+                                $("<div class='luckyDream'><div class='dream_logo'>"+item.pool.tbill / 100+"</div><div class='dream_right'><div class='dream_msg'><span>"+item.title+"</span><div class='icon_success'></div></div> <div class='tip'><span class='tip_text'>"+item.pool.ptitle+"</span></div></div></div>").appendTo('.dream_list')
                             }
                         })
-                        
+                        console.log(data)
+                        $('.luckyDream').click(function(){
+                            window.location.href = 'http://tinydream.antit.top/TinydreamWeb/html/luckyDream.html'
+                        })
                     }
                 }
             },function(code,data){
