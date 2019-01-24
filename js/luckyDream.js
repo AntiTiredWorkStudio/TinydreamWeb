b4a64242ae967e5cb6bf481234c480d7c73158f4
whugobarry <17600740311@163.com>
2019-01-24 20:48:31 +0800

    提交

--- js/luckyDream.js
+++ js/luckyDream.js
@@ -1,7 +1,7 @@
 $(function(){
     var lucky = JSON.parse(localStorage.getItem('lucky'));
     var did = localStorage.getItem('did');
-    $('.lucky_tip').html('恭喜您在：'+lucky.lottery.lid+'期'+did+'个梦想'+lucky.lottery.lid+'编号成为幸运编号，本期互助总资金为'+lucky.lottery.ubill / 100+'元')
+    $('.lucky_tip').html('恭喜您在：'+lucky.lottery.lid+'期第'+did+'个梦想'+lucky.lottery.lid+'编号成为幸运编号，本期互助总资金为'+lucky.pool.ubill / 100+'元')
     $('.btn').click(function(){
         localStorage.setItem('dr','{did:'+lucky.did+',state:"all"}')
         window.location.href = 'http://tinydream.antit.top/TinydreamWeb/html/add.html'
