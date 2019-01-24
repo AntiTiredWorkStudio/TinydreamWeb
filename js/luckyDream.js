3738b19b3bddc9b39b27fc139a74021c20b34a1d
whugobarry <17600740311@163.com>
2019-01-24 20:44:20 +0800

    提交

--- js/luckyDream.js
+++ js/luckyDream.js
@@ -1,5 +1,5 @@
 $(function(){
-    var lucky = JSON.stringify(localStorage.getItem('lucky'));
+    var lucky = JSON.parse(localStorage.getItem('lucky'));
     var did = localStorage.getItem('did');
     $('.lucky_tip').html('恭喜您在：'+lucky.lottery.lid+'期'+did+'个梦想'+lucky.lottery.lid+'编号成为幸运编号，本期互助总资金为'+lucky.lottery.ubill / 100+'元')
     $('.btn').click(function(){
