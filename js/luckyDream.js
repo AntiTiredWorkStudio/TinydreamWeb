cf550df6722a447a896654fc54521b1199271303
whugobarry <17600740311@163.com>
2019-01-25 19:34:55 +0800

    优化通知跳转

--- js/luckyDream.js
+++ js/luckyDream.js
@@ -1,4 +1,5 @@
 $(function(){
+    var userInfo = Options.GetUserInfo();
     var lucky = JSON.parse(localStorage.getItem('lucky'));
     var did = localStorage.getItem('did');
     $('.lucky_tip').html('恭喜您在：'+lucky.lottery.lid+'期第'+did+'个梦想'+lucky.lottery.lid+'编号成为幸运编号，本期互助总资金为'+lucky.pool.cbill / 100+'元')
@@ -6,8 +7,15 @@ $(function(){
 		$('.btn').css("display","none");
 	}else{
 		$('.btn').click(function(){
-			localStorage.setItem('dr','{"did":"'+lucky.did+'","state":"all"}')
-			window.location.href = 'http://tinydream.antit.top/TinydreamWeb/html/add.html'
+            TD_Request('us','rnamegx',{
+                uid:userInfo.openid
+            },function(code,data){
+                console.log(data)
+            },function(code,data){
+                console.log(data)
+            })
+			// localStorage.setItem('dr','{"did":"'+lucky.did+'","state":"all"}')
+			// window.location.href = 'http://tinydream.antit.top/TinydreamWeb/html/add.html'
 		})
 	}
     $('.look').click(function(){
