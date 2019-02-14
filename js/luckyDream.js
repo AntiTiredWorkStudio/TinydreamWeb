a858a9ec780b076c3aeafc3875092e96dbbbe79a
whugobarry <17600740311@163.com>
2019-02-14 23:01:10 +0800

    文案修改

--- js/luckyDream.js
+++ js/luckyDream.js
@@ -2,7 +2,7 @@ $(function(){
     var userInfo = Options.GetUserInfo();
     var lucky = JSON.parse(localStorage.getItem('lucky'));
     var did = localStorage.getItem('did');
-    $('.lucky_tip').html('恭喜您在：'+lucky.lottery.lid+'期第'+did+'个梦想'+lucky.lottery.lid+'编号成为幸运编号，本期互助总资金为'+lucky.pool.cbill / 100+'元')
+    $('.lucky_tip').html('恭喜您在：'+lucky.lottery.pid+'期第'+did+'个梦想'+lucky.lottery.lid+'编号成为幸运编号，本期互助总资金为'+lucky.pool.cbill / 100+'元')
 	if(lucky.state!="DOING"){
 		$('.btn').css("display","none");
 	}else{
