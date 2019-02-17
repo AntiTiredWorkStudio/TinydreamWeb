fda0eca0b3afb7ea48df8cbad90d4f5d523a4942
whugobarry <17600740311@163.com>
2019-02-17 12:22:41 +0800

    梦想红包页面

---
+++ js/RedEnvelope.js
@@ -0,0 +1,19 @@
+$(function(){
+    // 红包记录处理
+    $('.record span').click(function(){
+        window.location.href = 'http://tinydream.antit.top/TinydreamWeb/html/R_record.html'
+    })
+    // 普通红包处理
+    $('.ordinary').click(function(){
+        window.location.href = 'http://tinydream.antit.top/TinydreamWeb/html/R_payInfo.html'
+    })
+    // 红包规则处理
+    $('.rules span').click(function(){
+        window.location.href = 'http://tinydream.antit.top/TinydreamWeb/html/R_rules.html'
+    })
+    // 手气红包处理
+    $('.luck').click(function(){
+        alert('手气红包尚未上线，敬请期待……');
+        return;
+    })
+})
