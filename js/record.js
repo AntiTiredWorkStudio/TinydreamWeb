33d4d06a171e0813b6bf377b5421cd5435847d75
whugobarry <17600740311@163.com>
2019-02-18 18:25:54 +0800

    红包记录页面

--- js/record.js
+++ js/record.js
@@ -1,23 +1,24 @@
 $(function(){
     // 红包记录逻辑简单处理
     // 类型切换
-    var userInfo = Options.GetUserInfo();
-    // 用户头像
-    $('.headicon').css('background','url('+userInfo.headicon+') no-repeat')
-    $('.top ul li').click(function(){
-        $(this).addClass('active').siblings().removeClass('active')
-    })
-    // 收到红包
-    $('.r_left').click(function(){  
-        $('.tip').html(userInfo.nickname+'收到的梦想红包共')
-        $('.count').html('15')
-        $('.txt').html('个')
-        $('.tip_txt').html('')
-    })
-    $('.r_right').click(function(){  
-        $('.tip').html(userInfo.nickname+'发出的梦想红包共')
-        $('.count').html('15')
-        $('.txt').html('元')
-        $('.tip_txt').html('发出红包总数<span style="color:#f25542">15</span>个')
-    })
+    console.log(Options.GetUserInfo())
+    // var userInfo = Options.GetUserInfo();
+    // // 用户头像
+    // $('.headicon').css('background','url('+userInfo.headicon+') no-repeat')
+    // $('.top ul li').click(function(){
+    //     $(this).addClass('active').siblings().removeClass('active')
+    // })
+    // // 收到红包
+    // $('.r_left').click(function(){  
+    //     $('.tip').html(userInfo.nickname+'收到的梦想红包共')
+    //     $('.count').html('15')
+    //     $('.txt').html('个')
+    //     $('.tip_txt').html('')
+    // })
+    // $('.r_right').click(function(){  
+    //     $('.tip').html(userInfo.nickname+'发出的梦想红包共')
+    //     $('.count').html('15')
+    //     $('.txt').html('元')
+    //     $('.tip_txt').html('发出红包总数<span style="color:#f25542">15</span>个')
+    // })
 })
