545a43ddf8cd1850ce51432814539e5140937b66
whugobarry <17600740311@163.com>
2019-02-22 11:13:05 +0800

    分享优化

--- js/payInfo.js
+++ js/payInfo.js
@@ -5,7 +5,7 @@ if (!ExistStorage("buy")) {
 } else {
     $(function () {
         var imgArr = ['zhang','wang'];
-        console.log(Math.floor(Math.random()-1*imgArr.length))
+        console.log(Math.floor(Math.random()*imgArr.length))
         var pay = null;
         var userInfo = Options.GetUserInfo();
         var buy = JSON.parse(localStorage.getItem('buy'));
