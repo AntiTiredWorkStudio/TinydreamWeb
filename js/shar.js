fef07ef3ed9f8de4ab68e351c9a6134da028b041
whugobarry <17600740311@163.com>
2019-02-22 13:20:17 +0800

    分享优化

--- js/shar.js
+++ js/shar.js
@@ -5,10 +5,15 @@ $(function(){
         background:'url('+img+') no-repeat',
         'background-size':'cover'
     });
+    var imgs = new Image();
+    imgs.src = img;
     // 获取画布
     var canvas = document.getElementById('canvas')
     canvas.style.width = $('.share').width();
     canvas.style.height = $('.share').height();
     // 获取图形上下文
     var ctx = canvas.getContext('2d');
+    ctx.beginPath();
+    ctx.drawImage(imgs,0,0,$('.share').width(),$('.share').height())
+    
 })
