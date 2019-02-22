cdc26ca31c6fa59750a584ffd5c4b185f3f34d3b
whugobarry <17600740311@163.com>
2019-02-22 13:10:29 +0800

    分享优化

--- js/shar.js
+++ js/shar.js
@@ -5,4 +5,10 @@ $(function(){
         background:'url('+img+') no-repeat',
         'background-size':'cover'
     });
+    // 获取画布
+    var canvas = document.getElementById('canvas')
+    canvas.style.width = $('.share').width();
+    canvas.style.height = $('.share').height();
+    // 获取图形上下文
+    var ctx = canvas.getContext('2d');
 })
