e82b8e62dd216dc8f03b4c3d06de748fe22db30c
whugobarry <17600740311@163.com>
2019-02-22 13:23:59 +0800

    分享优化

--- js/shar.js
+++ js/shar.js
@@ -9,11 +9,11 @@ $(function(){
     imgs.src = img;
     // 获取画布
     var canvas = document.getElementById('canvas')
-    canvas.style.width = $('.share').width();
-    canvas.style.height = $('.share').height();
+    canvas.width = $('.share').width();
+    canvas.height = $('.share').height();
     // 获取图形上下文
     var ctx = canvas.getContext('2d');
     ctx.beginPath();
     ctx.drawImage(imgs,0,0,$('.share').width(),$('.share').height())
-    
+    $('.share').css('background','url(unknow)')
 })
