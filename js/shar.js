0c86208aa8bfdcb1c7ff1b2e8bd57e2261ea7ea9
whugobarry <17600740311@163.com>
2019-02-22 12:18:23 +0800

    分享优化

--- js/shar.js
+++ js/shar.js
@@ -1,4 +1,4 @@
 $(function(){
     console.log($('body').height());
-    $('body').css({'background':'url(../images/1.png) no-repeat,background-size:cover'});
+    $('.share').css({'background':'url(../images/1.png) no-repeat,background-size:cover'});
 })
