95225c2db64f533b025d62f391150ba86cbc4fce
whugobarry <17600740311@163.com>
2019-02-24 05:42:15 +0800

    分享优化

--- js/shar.js
+++ js/shar.js
@@ -49,7 +49,21 @@ $(function () {
                 height = 60;
                 ctx.drawImage(img,x,y,width,height);
                 canvas = document.getElementById('canvas');
-                canvas2image();
+                ctx.globalCompositeOperation = 'source-over'
+                ctx.fillStyle = '#fff';
+                ctx.font = '15px 微软雅黑';
+                if(userInfo.nickname.length > 6){
+                    var name = userInfo.nickname.substring(0,7)+'...';
+                }else{
+                    var name = userInfo.nickname;
+                }
+                ctx.fillText(name + ' #2019小梦想#',75,56);
+                var dream = '云南鲜花饼店...';
+                ctx.font = '19px 微软雅黑'
+                ctx.fillText(dream+'(已成功参与互助)',75,88);
+                ctx.font = '14px 微软雅黑';
+                ctx.fillText('扫码关注',x2,y1);
+                ctx.fillText('小梦想互助',x3,y);
                
             }
             
@@ -103,31 +117,17 @@ $(function () {
             // }
         // }
     // }
-    function canvas2image(){
-        ctx.globalCompositeOperation = 'source-over'
-        ctx.fillStyle = '#fff';
-        ctx.font = '15px 微软雅黑';
-        if(userInfo.nickname.length > 6){
-            var name = userInfo.nickname.substring(0,7)+'...';
-        }else{
-            var name = userInfo.nickname;
-        }
-        ctx.fillText(name + ' #2019小梦想#',75,56);
-        var dream = '云南鲜花饼店...';
-        ctx.font = '19px 微软雅黑'
-        ctx.fillText(dream+'(已成功参与互助)',75,88);
-        ctx.font = '14px 微软雅黑';
-        ctx.fillText('扫码关注',x2,y1);
-        ctx.fillText('小梦想互助',x3,y);
-        // var imgs = new Image();
-    //    imgs.src = canvas.toDataURL('image/png');
-    //    imgs.width = $('.share').width();
-    //     imgs.height = $('.share').height();
-        // var imgs =new Image();
-        // imgs.src = canvas.toDataURL('image/jpg',1);
-        // var url1 = canvas.toDataURL('image/jpg',1)
-        // return canvas;
-    }
+    // function canvas2image(){
+        
+    //     // var imgs = new Image();
+    // //    imgs.src = canvas.toDataURL('image/png');
+    // //    imgs.width = $('.share').width();
+    // //     imgs.height = $('.share').height();
+    //     // var imgs =new Image();
+    //     // imgs.src = canvas.toDataURL('image/jpg',1);
+    //     // var url1 = canvas.toDataURL('image/jpg',1)
+    //     // return canvas;
+    // }
     $('.bg').attr('src',url).css({
         width:$('.share').width(),
         height:$('.share').height(),
@@ -141,25 +141,5 @@ $(function () {
     }else{
         $('.nickname .name').html(userInfo.nickname);
     }
-    // $('#canvas').remove()
-    // var imgurl = canvas.toDataURL('image/png');
-    // alert(2)
-    // document.getElementById('share').innerHTML = '<img class="img" src="'+imgurl+'" />';
-    // $('.img').width($('.share').width())
-    // $('.img').height($('.share').height())
-    // html转为图片
-//   setTimeout(function(){
-//         html2canvas(document.getElementById('share')).then(function(canvas){
-//             var url = canvas.toDataURL('image/png');
-//             $('#share').html('<img src='+url+' />');
-//             $('img').width($('.share').width());
-//             $('img').height($('.share').height())
-//             alert(url)
-//         })
-//     },10000)
-$('.share').click(function(){
-    var url = canvas.toDataURL('image/png');
-    $('.share').html('<img src="'+url+'">')
-})
 
 })
