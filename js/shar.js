1497d1dd830d13352ee9f594f2b226c0a94a0ba8
whugobarry <17600740311@163.com>
2019-02-23 15:02:57 +0800

    分享优化

--- js/shar.js
+++ js/shar.js
@@ -15,10 +15,8 @@ $(function(){
     // 获取图形上下文
     var ctx = canvas.getContext('2d');
     async function draw(){
-        imgs.onload = async function(){
-            ctx.beginPath();
-            await ctx.drawImage(imgs,0,0,$('.share').width(),$('.share').height()) 
-        }
+        ctx.beginPath();
+        await ctx.drawImage(imgs,0,0,$('.share').width(),$('.share').height()) 
         alert(canvas.toDataURL("image/png"))
         $('img').attr('src',canvas.toDataURL("image/png"))
         $('#canvas').remove();
