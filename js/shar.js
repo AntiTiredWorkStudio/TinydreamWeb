1c8c1fd861495b457c535c4d030f4e0f624cf8a3
whugobarry <17600740311@163.com>
2019-02-24 05:28:10 +0800

    分享优化

--- js/shar.js
+++ js/shar.js
@@ -49,12 +49,33 @@ $(function () {
                 height = 60;
                 ctx.drawImage(img,x,y,width,height);
                 canvas = document.getElementById('canvas');
-                canvas2image(canvas).then(function(res){
-                    alert(res)
-                    $('.share').html(res)
-                    alert('success')
-                })
-               
+                // canvas2image(canvas).then(function(res){
+                //     alert(res)
+                //     $('.share').html(res)
+                //     alert('success')
+                // })
+                ctx.globalCompositeOperation = 'source-over'
+                ctx.fillStyle = '#fff';
+                ctx.font = '15px 微软雅黑';
+                if(userInfo.nickname.length > 6){
+                    var name = userInfo.nickname.substring(0,7)+'...';
+                }else{
+                    var name = userInfo.nickname;
+                }
+                ctx.fillText(name + ' #2019小梦想#',75,56);
+                var dream = '云南鲜花饼店...';
+                ctx.font = '19px 微软雅黑'
+                ctx.fillText(dream+'(已成功参与互助)',75,88);
+                ctx.font = '14px 微软雅黑';
+                ctx.fillText('扫码关注',x2,y1);
+                ctx.fillText('小梦想互助',x3,y);
+                // var imgs = new Image();
+    //            imgs.src = canvas.toDataURL('image/png');
+    //            imgs.width = $('.share').width();
+    //             imgs.height = $('.share').height();
+                var imgs =new Image();
+                imgs.src = canvas.toDataURL('image/jpg',1);
+                alert(imgs)
             }
             
         }
@@ -108,62 +129,7 @@ $(function () {
         // }
     // }
     async function canvas2image(canvas){
-        // var num = 0;
-        // for (var i = 0; i<)
-        // img.src = imgArr[num];
-        img.onload = function(){
-            // if(num == 0){
-            //     ctx.drawImage(img,0,0,$('.share').width(),$('.share').height());
-            //     num++;
-            // }
-        }
-
-        // var bg = new Image();
-        // bg.src = url;
-        // var head = new Image();
-        // head.src = userInfo.headimgurl;
-        // var qr = new Image();
-        // qr.src = 'http://tdream.antit.top/LongPress2ShareQR.jpg';
-        // bg.setAttribute('crossOrigin','anonymous')
-        // bg.onload =  function(){
-        //     // ctx.globalCompositeOperation = 'xor'
-        //      ctx.drawImage(bg,0,0,$('.share').width(),$('.share').height());
-        //      canvas.toDataURL('image/png');
-        //      $('.share').html('<img src='+canvas.toDataURL('image/png')+'>')
-        // }
-        // head.onload =  function(){
-        //     ctx.globalCompositeOperation = 'destination-over'
-        //     bg.setAttribute('crossOrigin','anonymous')
-        //     // ctx.globalCompositeOperation = 'xor'
-        //      ctx.drawImage(head,15,40,50,50);
-        //      $('.share').html('<img src='+canvas.toDataURL('image/png')+'>')
-        // }
-        // qr.onload =  function(){
-        //     bg.setAttribute('crossOrigin','anonymous')
-        //     ctx.globalCompositeOperation = 'destination-over';
-        //      ctx.drawImage(qr,x1,y2,60,60);
-        // }
-        ctx.globalCompositeOperation = 'source-over'
-        ctx.fillStyle = '#fff';
-        ctx.font = '15px 微软雅黑';
-        if(userInfo.nickname.length > 6){
-            var name = userInfo.nickname.substring(0,7)+'...';
-        }else{
-            var name = userInfo.nickname;
-        }
-        ctx.fillText(name + ' #2019小梦想#',75,56);
-        var dream = '云南鲜花饼店...';
-        ctx.font = '19px 微软雅黑'
-        ctx.fillText(dream+'(已成功参与互助)',75,88);
-        ctx.font = '14px 微软雅黑';
-        ctx.fillText('扫码关注',x2,y1);
-        ctx.fillText('小梦想互助',x3,y);
-        // var imgs = new Image();
-    //    imgs.src = canvas.toDataURL('image/png');
-    //    imgs.width = $('.share').width();
-    //     imgs.height = $('.share').height();
-        var imgs =new Image();
-        imgs.src = canvas.toDataURL('image/jpg',1);
+        
         var url1 = canvas.toDataURL('image/jpg',1)
         return imgs;
     }
