8fa8238b680efd49b14a8d7c1a57d3fb4628cb33
whugobarry <17600740311@163.com>
2019-02-23 15:52:37 +0800

    分享优化

--- js/shar.js
+++ js/shar.js
@@ -1,26 +1,13 @@
-$(function(){
-    var url;
-    console.log($('body').height());
-    var img = localStorage.getItem('img');
+$(function () {
+    var url = localStorage.getItem('img');
+    var userInfo = Options.GetUserInfo();
     $('.share').css({
-        background:'url('+img+') no-repeat',
+        background:'url('+url+') no-repeat',
         'background-size':'cover'
-    });
-    // 获取画布
-    var canvas = document.getElementById('canvas')
-    canvas.width = $('.share').width();
-    canvas.height = $('.share').height();
-    // 获取图形上下文
-    var ctx = canvas.getContext('2d');
-    var imgs = new Image();
-
-    async function draw(){
-        ctx.beginPath();
-        imgs.onload = function () {await ctx.drawImage(imgs,0,0,$('.share').width(),$('.share').height())} 
-        await alert(canvas.toDataURL("image/png"))
-        await $('img').attr('src',canvas.toDataURL("image/png"))
-        $('#canvas').remove();
-    }
-    draw();
-    console.log(url);
+    })
+    $('.headicon').css({
+        'background':'url('+userInfo.headimgurl+') no-repeat',
+        'background-size':'1rem 1rem'
+    })
+    $('.nickname .name').html(userInfo.nickname);
 })
