7ce3d266aee0cb4a7017e12ba8f82d717eb7ca16
whugobarry <17600740311@163.com>
2019-02-23 15:18:15 +0800

    分享优化

--- js/shar.js
+++ js/shar.js
@@ -13,15 +13,14 @@ $(function(){
     // 获取图形上下文
     var ctx = canvas.getContext('2d');
     var imgs = new Image();
-    ctx.beginPath();
-    imgs.onload = function () {
-        ctx.drawImage(imgs,0,0,$('.share').width(),$('.share').height())
-        img(canvas.toDataURL('image/png'))
-    } 
-    function img(url){
-        alert(canvas.toDataURL("image/png"))
-        $('img').attr('src',canvas.toDataURL("image/png"))
+
+    async function draw(){
+        ctx.beginPath();
+        imgs.onload = function () {await ctx.drawImage(imgs,0,0,$('.share').width(),$('.share').height())} 
+        await alert(canvas.toDataURL("image/png"))
+        await $('img').attr('src',canvas.toDataURL("image/png"))
         $('#canvas').remove();
     }
+    draw();
     console.log(url);
 })
