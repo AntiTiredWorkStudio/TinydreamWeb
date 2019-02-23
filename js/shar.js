979a2ea98ce798762ac354108be65356c463ab09
whugobarry <17600740311@163.com>
2019-02-23 15:14:05 +0800

    分享优化

--- js/shar.js
+++ js/shar.js
@@ -6,21 +6,22 @@ $(function(){
         background:'url('+img+') no-repeat',
         'background-size':'cover'
     });
-    var imgs = new Image();
-    imgs.src = img;
     // 获取画布
     var canvas = document.getElementById('canvas')
     canvas.width = $('.share').width();
     canvas.height = $('.share').height();
     // 获取图形上下文
     var ctx = canvas.getContext('2d');
-    async function draw(){
-        ctx.beginPath();
-        await ctx.drawImage(imgs,0,0,$('.share').width(),$('.share').height()) 
+    var imgs = new Image();
+    ctx.beginPath();
+    imgs.onload = function () {
+        ctx.drawImage(imgs,0,0,$('.share').width(),$('.share').height())
+        img(canvas.toDataURL('image/png'))
+    } 
+    function img(url){
         alert(canvas.toDataURL("image/png"))
         $('img').attr('src',canvas.toDataURL("image/png"))
         $('#canvas').remove();
     }
-    draw();
     console.log(url);
 })
