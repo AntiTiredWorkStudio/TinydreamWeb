8f928c5f2c7414d68136f53171bb2784c8af0aeb
whugobarry <17600740311@163.com>
2019-02-23 22:23:10 +0800

    分享优化

--- js/shar.js
+++ js/shar.js
@@ -1,5 +1,16 @@
 $(function () {
     var url = localStorage.getItem('img');
+    var canvas = document.getElementById('canvas');
+    canvas.width = $('.share').width();
+    canvas.height = $('.share').height();
+    var ctx = canvas.getContext('2d');
+    ctx.globalCompositeOperation = 'source-over'
+    var x1 = canvas.width - 75;
+    var x2 = canvas.width - 141;
+    var x3 = canvas.width - 155;
+    var y = canvas.height - 25;
+    var y2 = canvas.height - 85;
+    var y1 = canvas.height - 44;
     var userInfo = Options.GetUserInfo();
     $('.bg').attr('src',url).css({
         width:$('.share').width(),
@@ -15,21 +26,22 @@ $(function () {
         $('.nickname .name').html(userInfo.nickname);
     }
 
-    var canvas = document.getElementById('canvas');
-    canvas.width = $('.share').width();
-    canvas.height = $('.share').height();
-    var ctx = canvas.getContext('2d');
     var bg = new Image();
     bg.src = url;
     var head = new Image();
     head.src = userInfo.headimgurl;
     var qr = new Image();
     qr.src = 'https://tdream.antit.top/LongPress2ShareQR.jpg';
-    $(window).load(function(){
+    bg.onload = function(){
         ctx.drawImage(bg,0,0,$('.share').width(),$('.share').height())
-        ctx.globalCompositeOperation = 'source-over'
-        // ctx.drawImage(head,0,0,50,50);
+    }
+    head.onload = function(){
         ctx.drawImage(head,15,40,50,50);
+    }
+    qr.onload = function(){
+        ctx.drawImage(qr,x1,y2,60,60);
+    }
+    $(window).load(function(){
         ctx.fillStyle = '#fff';
         ctx.font = '15px 微软雅黑';
         if(userInfo.nickname.length > 6){
@@ -41,24 +53,12 @@ $(function () {
         var dream = '云南鲜花饼店...';
         ctx.font = '19px 微软雅黑'
         ctx.fillText(dream+'(已成功参与互助)',75,88);
-        ctx.font = '28px 微软雅黑';
-        var x1 = canvas.width - 75;
-        var x2 = canvas.width - 141;
-        var x3 = canvas.width - 155;
-        var y = canvas.height - 25;
-        var y2 = canvas.height - 85;
-        var y1 = canvas.height - 44;
         ctx.font = '14px 微软雅黑';
         ctx.fillText('扫码关注',x2,y1);
         ctx.fillText('小梦想互助',x3,y);
-        ctx.drawImage(qr,x1,y2,60,60);
         var url = canvas.toDataURL('image/png');
         document.getElementById('share').innerHTML = '<img src="'+url+'" />';
         $('.bg').remove();
-        // $('#canvas').remove()
-        alert('success');
-        // alert(1)
-        
     })
     // html转为图片
 //   setTimeout(function(){
