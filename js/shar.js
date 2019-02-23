dd15a56e1d7c42ecb8eedc6be6f7cce018916c0e
whugobarry <17600740311@163.com>
2019-02-24 04:02:05 +0800

    分享优化

--- js/shar.js
+++ js/shar.js
@@ -41,10 +41,10 @@ $(function () {
                 num++;
                 if(total+1 == num){
                     alert('加载完毕');
-                    // ctx.drawImage(img[0],0,0,$('.share').width(),$('.share').height())
-                    // ctx.drawImage(img[1],15,40,50,50);
-                    // ctx.drawImage(img[2],x1,y2,60,60);
-                    $('.share').html(1)
+                    ctx.drawImage(img[0],0,0,$('.share').width(),$('.share').height())
+                    ctx.drawImage(img[1],15,40,50,50);
+                    ctx.drawImage(img[2],x1,y2,60,60);
+                    // $('.share').html(1)
                 }
                 // ctx.globalCompositeOperation = 'source-over'
                 // if(i == 0){
