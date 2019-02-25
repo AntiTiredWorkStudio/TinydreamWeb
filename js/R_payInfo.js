03ddb4935fa56e8f4bf27bc9e027ccd567d211e4
whugobarry <17600740311@163.com>
2019-02-25 11:41:52 +0800

    发送红包主要逻辑

--- js/R_payInfo.js
+++ js/R_payInfo.js
@@ -1,6 +1,16 @@
 $(function(){
+    // 获取用户信息
+    var userInfo = Options.GetUserInfo();
     // 获取奖池
-    TD_Request('ds','plistr',function(code,data){
+    TD_Request('ds','plistr',{
+        seek:0,
+        count:100
+    },function(code,data){
         console.log(data)
+        var pid = [];
+        $.each(data.Pools,function(index,item){
+            console.log(item);
+        })
+        // weui.packer()
     })
 })
