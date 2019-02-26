a87bfb9188abd1829893dc804b971cc089a97f5b
whugobarry <17600740311@163.com>
2019-02-26 14:07:13 +0800

    红包逻辑补充

---
+++ js/share.js
@@ -0,0 +1,11 @@
+try {
+	var nickname = Options.GetUserInfo().nickname;
+	WebApp.JSAPI.Init({
+		title:nickname+"给你发了一个梦想红包,领取可夺大额梦想金！",
+		desc:"小梦想互助——让每个小梦想 都有机会实现",
+		link:'http://tinydream.antit.top/TinydreamWeb/html/getR_Pack.html?rid='+$_GET.rid,
+		imgUrl:"https://tdream.antit.top/image/Red_Open.jpg"
+	});
+} catch (error) {
+	alert(error)
+}
