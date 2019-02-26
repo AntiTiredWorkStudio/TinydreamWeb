fc792b9af6deb299394f6ff709dbcf57b145eed4
whugobarry <17600740311@163.com>
2019-02-26 19:54:49 +0800

    红包逻辑补充

--- js/record.js
+++ js/record.js
@@ -2,25 +2,30 @@ $(function(){
     // 红包记录逻辑简单处理
     // 类型切换
     var userInfo = Options.GetUserInfo();
-    get('get','gurpr',0);
-
-    // 获取红包函数
-    /*
-    参数 redpack 请求状态
-    参数 rp 红包状态（gurpr:收到红包;gurps发出红包）
-    参数 num 游标
-    */
-    function get(redpack,rp,num){
-        TD_Request('rp',rp,{
+    redpack('get','gurpr',0)
+    /**
+     * 参数state 红包请求状态
+     * 参数actions 请求动作
+     * 参数num 游标
+     */
+    function redpack(state,actions,num){
+        TD_Request('rp',actions,{
             uid:userInfo.openid,
             seek:num,
-            count:5,
+            count:5
         },function(code,data){
             console.log(data)
+            $('.count').html(data.stats.countPack)
+            if(state == 'get'){
+                $('.tip_txt').html('收到红包金额<span style="color:#f25542">'+data.stats.totalBill * 100+'</span>元').css('font-size','0.3rem');
+            }else{
+                $('.tip_txt').html('发出红包金额<span style="color:#f25542">'+data.stats.totalBill * 100+'</span>元').css('font-size','0.3rem');
+            }
         },function(code,data){
             console.log(data)
-        })  
+        })
     }
+    
     console.log(Options.GetUserInfo())
     
     // 用户头像
@@ -31,13 +36,12 @@ $(function(){
     })
     // 收到红包
     $('.r_left').click(function(){  
-        $('.tip_txt').html('');
         $('.tip').html(userInfo.nickname+'收到的梦想红包共')
-        get('get','gurpr',0)
+        redpack('get','gurpr',0)
 
     })
     $('.r_right').click(function(){  
-        get('give','gurps',0);
+        redpack('give','gurps',0);
         $('.tip').html(userInfo.nickname+'发出的梦想红包共')
     })
 })
