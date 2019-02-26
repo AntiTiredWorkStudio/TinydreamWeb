a5e06e53ca24f50167795a963c003189513f6810
whugobarry <17600740311@163.com>
2019-02-27 02:10:45 +0800

    梦想红包领取

--- js/GetRedPack.js
+++ js/GetRedPack.js
@@ -23,17 +23,16 @@ $(function(){
     })
     TD_Request('rp','orp',{uid:userInfo.openid,rid:rinfo.rid},function(code,data){
         console.log(data);
-        // $('.get_num').html('已领取编号');
-        // var lid;
-        // $.each(data.nums,function(index,item){
-        //     console.log(item);
-        //     lid = item.lid;
-        // })
-        // $('.number').html(lid);
-        // $('button').html('查看详情');
-        // $('button').click(function(){
-        //     window.location.href = 'http://tinydream.antit.top/TinydreamWeb/html/record.html'
-        // })
+        var lid;
+        $.each(data.nums,function(index,item){
+            // console.log(item);
+            lid = item.lid;
+        })
+        $('.get_num').html('已领取编号：<span class="number">'+lid+'</span>');
+        $('.r_tip').html('该编号自动参与小梦想互助平台'+data.pid+'期的幸运者抓阄，幸运者可以领取'+data.totalBill/100+'元梦想互助金去启动梦想')
+        $('button').html('查看详情').click(function(){
+            mainPool(data.pid);
+        })
     },function(code,data){
         console.log(data)
         if(code == 72){
@@ -42,32 +41,23 @@ $(function(){
             $('button').html('查看详情').click(function(){
                 mainPool(data.pid);
             })
-        }
-        // if(code == 11){
-        //     $('.get_num').html('待取编号');
-        //     $('.number').html('1个');
-        //     $('button').html('绑定手机');
-        //     $('button').click(function(){
-        //         window.location.href = 'http://tinydream.antit.top/TinydreamWeb/html/phoneManage.html?phone=false'
-        //     })
-        // }else if(code == 71){
-        //     $('button').html('添加梦想');
-        //     $('button').click(function(){
-        //         window.location.href = 'http://tinydream.antit.top/TinydreamWeb/html/dream.html?dream=false'
-        //     })
-        // }else if(code == 72){
-        //     $('.get_num').html('已领取编号');
-        //     $('.number').html(data.reco.lid);
-        //     $('button').html('查看详情');
-        //     $('button').click(function(){
-        //         window.location.href = 'http://tinydream.antit.top/TinydreamWeb/html/record.html'
-        //     })
-        // }else if(code == 18){
-        //     alert('您当日购买次数已达上限,无法领取该红包');
-        //     return;
-        // }else if(code== 5){
-        //     alert(data.context);
-        //     return;
-        // }
+        }else if(code == 11){
+            $('.get_num').html('待取编号:1个');
+            $('.r_tip').html('该编号自动参与小梦想互助平台--期的幸运者抓阄，幸运者可以领取--元梦想互助金去启动梦想')
+            $('button').html('绑定手机').click(function(){
+                window.location.href = 'http://tinydream.antit.top/TinydreamWeb/html/phoneManage.html?phone=false'
+            })
+        }else if(code == 71){
+            $('.r_tip').html('该编号自动参与小梦想互助平台--期的幸运者抓阄，幸运者可以领取--元梦想互助金去启动梦想')
+            $('button').html('添加梦想').click(function(){
+                window.location.href = 'http://tinydream.antit.top/TinydreamWeb/html/dream.html?dream=false'
+            })
+        }else if(code == 18){
+            alert('您当日购买次数已达上限,无法领取该红包');
+            window.location.href = 'http://tinydream.antit.top/TinydreamWeb/index.html'
+        }else if(code== 5){
+            alert(data.context);
+            window.location.href = 'http://tinydream.antit.top/TinydreamWeb/index.html'
+        }  
     })
 })
--- js/share.js
+++ js/share.js
@@ -8,12 +8,14 @@ try {
 	TD_Request('rp','grp',{rid:rid},function(code,data){
 		console.log(data)
 		var nickname = data.sender.nickname;
+		$('.title').html(nickname+'给你发了一个梦想红包,领取可夺大额梦想金！')
 		WebApp.JSAPI.Init({
 			title:nickname+"给你发了一个梦想红包,领取可夺大额梦想金！",
 			desc:"小梦想互助——让每个小梦想 都有机会实现",
 			link:'http://tinydream.antit.top/TinydreamWeb/html/getR_Pack.html?rid='+rid,
 			imgUrl:"https://tdream.antit.top/image/Red_Open.jpg"
 		});
+		
 	},function(code,data){
 		console.log(data)
 	})
