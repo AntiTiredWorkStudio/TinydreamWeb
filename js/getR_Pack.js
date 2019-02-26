1e05659145640cf01d99aa85dd1f8481660bbb67
konglf2112 <konglf2112@foxmail.com>
2019-02-26 10:19:46 +0800

    增加红包打开页面的授权拉取
    Signed-off-by: konglf2112 <konglf2112@foxmail.com>

--- js/getR_Pack.js
+++ js/getR_Pack.js
@@ -1,19 +1,23 @@
 $(function(){
-    var rid = $_GET.rid;
-    TD_Request('rp','grp',{rid:rid},function(code,data){
-        console.log(data)
-        var userInfo = data.sender;
-        $('.headicon').css({
-            background:'url('+userInfo.headicon+') no-repeat',
-            'background-size':'0.88rem 0.88rem'
-        });
-        $('.title').html(userInfo.nickname+'的梦想红包');
-        $('.msg').html(data.redpack.content);
-        $('button').click(function(){
-            localStorage.setItem('rinfo',JSON.stringify({rid:rid,headicon:userInfo.headicon,nickname:userInfo.nickname,content:data.redpack.content}));
-            window.location.href = 'http://tinydream.antit.top/TinydreamWeb/html/GetRedPack.html'
-        })
-    },function(code,data){
-        console.log(data)
-    })
+	WebApp.Init('wxc5216d15dd321ac5',//appid
+        function(result,data){
+			var rid = $_GET.rid;
+			TD_Request('rp','grp',{rid:rid},function(code,data){
+				console.log(data)
+				var userInfo = data.sender;
+				$('.headicon').css({
+					background:'url('+userInfo.headicon+') no-repeat',
+					'background-size':'0.88rem 0.88rem'
+				});
+				$('.title').html(userInfo.nickname+'的梦想红包');
+				$('.msg').html(data.redpack.content);
+				$('button').click(function(){
+					localStorage.setItem('rinfo',JSON.stringify({rid:rid,headicon:userInfo.headicon,nickname:userInfo.nickname,content:data.redpack.content}));
+					window.location.href = 'http://tinydream.antit.top/TinydreamWeb/html/GetRedPack.html'
+				})
+			},function(code,data){
+				console.log(data)
+			})
+		}
+	);
 })
