7791896638b6f99ecf8991acd5bc52c44914a1f4
whugobarry <17600740311@163.com>
2019-02-27 00:00:46 +0800

    红包逻辑补充

--- js/share.js
+++ js/share.js
@@ -4,7 +4,7 @@ try {
 	WebApp.JSAPI.Init({
 		title:nickname+"给你发了一个梦想红包,领取可夺大额梦想金！",
 		desc:"小梦想互助——让每个小梦想 都有机会实现",
-		link:'http://tinydream.antit.top/TinydreamWeb/html/getR_Pack.html?rid=311573768238',
+		link:'http://tinydream.antit.top/TinydreamWeb/html/getR_Pack.html?rid='+$_GET.rid,
 		imgUrl:"https://tdream.antit.top/image/Red_Open.jpg"
 	});
 	WebApp.JSAPI.onShareTimeLine = function(res){
