72b96dbea901af310e5c56895e88aaa56df5972e
whugobarry <17600740311@163.com>
2019-02-27 00:53:30 +0800

    红包逻辑补充

--- js/share.js
+++ js/share.js
@@ -1,17 +1,17 @@
 try {
+	var rid = $_GET.rid;
 	if(localStorage.getItem('msg') == 'ok'){
 		localStorage.removeItem('msg');
 	}else{
-		window.location.href = 'http://tinydream.antit.top/TinydreamWeb/html/getR_Pack.html'
+		window.location.href = 'http://tinydream.antit.top/TinydreamWeb/html/getR_Pack.html?rid='+rid;
 	}
-	var rid = $_GET.rid;
 	TD_Request('rp','grp',{rid:rid},function(code,data){
 		console.log(data)
 		var nickname = data.sender.nickname;
 		WebApp.JSAPI.Init({
 			title:nickname+"给你发了一个梦想红包,领取可夺大额梦想金！",
 			desc:"小梦想互助——让每个小梦想 都有机会实现",
-			link:'http://tinydream.antit.top/TinydreamWeb/html/getR_Pack.html?rid='+$_GET.rid,
+			link:'http://tinydream.antit.top/TinydreamWeb/html/getR_Pack.html?rid='+rid,
 			imgUrl:"https://tdream.antit.top/image/Red_Open.jpg"
 		});
 	},function(code,data){
