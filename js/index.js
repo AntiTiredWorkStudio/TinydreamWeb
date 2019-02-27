4c83ed7fd6b60b26347b422eb7791ad326e6a894
whugobarry <17600740311@163.com>
2019-02-27 18:37:52 +0800

    页面优化

--- js/index.js
+++ js/index.js
@@ -22,8 +22,6 @@ $(function(){
                 pagination: {
                   el: '.swiper-pagination',
                 },
-                spaceBetween: 30,
-                centeredSlides: true,
                 autoplay: {
                     delay: 3000,
                     disableOnInteraction: false,
