3c761c10cc8a413dbb69e684e1681fe3029609f0
whugobarry <17600740311@163.com>
2019-02-28 14:12:32 +0800

    页面优化

--- js/GetRedPack.js
+++ js/GetRedPack.js
@@ -2,19 +2,19 @@ $(function(){
     var rinfo = JSON.parse(localStorage.getItem('rinfo'));
     var userInfo = Options.GetUserInfo();
     // 获取梦想池信息
-    function mainPool(pid){
-        TD_Request('dp','pinfo',{
-            pid:pid
-        },function(code,data){
-            var obj = DreamPoolAnalysis(data.pool)
-            localStorage.setItem('poolInfo',JSON.stringify(obj))
-            if(obj.state == 'RUNNING'){
-                window.location.href = "http://tinydream.antit.top/TinydreamWeb/html/helpInfo.html";
-            }else{
-                window.location.href = "http://tinydream.antit.top/TinydreamWeb/html/end.html";
-            }    
-        })
-    }
+    // function mainPool(pid){
+    //     TD_Request('dp','pinfo',{
+    //         pid:pid
+    //     },function(code,data){
+    //         var obj = DreamPoolAnalysis(data.pool)
+    //         localStorage.setItem('poolInfo',JSON.stringify(obj))
+    //         if(obj.state == 'RUNNING'){
+    //             window.location.href = "http://tinydream.antit.top/TinydreamWeb/html/helpInfo.html";
+    //         }else{
+    //             window.location.href = "http://tinydream.antit.top/TinydreamWeb/html/end.html";
+    //         }    
+    //     })
+    // }
     $('.title').html(rinfo.nickname+'的梦想红包');
     $('.msg').html(rinfo.content);
     if(rinfo.headicon == ''){
@@ -33,29 +33,35 @@ $(function(){
         })
         $('.get_num').html('已领取编号：<span class="number">'+lid+'</span>');
         $('.r_tip').html('该编号自动参与小梦想互助平台'+data.pid+'期的幸运者抓阄，幸运者可以领取'+data.totalBill/100+'元梦想互助金去启动梦想')
-        $('button').html('查看详情').click(function(){
-            mainPool(data.pid);
-        })
+        // $('button').html('查看详情').click(function(){
+        //     mainPool(data.pid);
+        // })
+        $('button').hide();
+        $('.hide').show();
     },function(code,data){
         console.log(data)
         if(code == 72){
             $('.get_num').html('已领取编号：<span class="number">'+data.reco.lid+'</span>');
             $('.r_tip').html('该编号自动参与小梦想互助平台'+data.pid+'期的幸运者抓阄，幸运者可以领取'+data.totalBill/100+'元梦想互助金去启动梦想')
-            $('button').html('查看详情').click(function(){
-                mainPool(data.pid);
-            })
+            // $('button').html('查看详情').click(function(){
+            //     mainPool(data.pid);
+            // })
+            $('button').hide();
+            $('.hide').show();  
         }else if(code == 11){
             $('.get_num').html('待取编号:1个');
             $('.r_tip').html('该编号自动参与小梦想互助平台--期的幸运者抓阄，幸运者可以领取--元梦想互助金去启动梦想')
-            $('button').html('绑定手机').click(function(){
+            $('button').show().html('绑定手机').click(function(){
                 window.location.href = 'http://tinydream.antit.top/TinydreamWeb/html/phoneManage.html?phone=false'
             })
+            $('.hide').hide();
         }else if(code == 71){
             $('.get_num').html('待取编号:1个');
             $('.r_tip').html('该编号自动参与小梦想互助平台--期的幸运者抓阄，幸运者可以领取--元梦想互助金去启动梦想')
-            $('button').html('添加梦想').click(function(){
+            $('button').show().html('添加梦想').click(function(){
                 window.location.href = 'http://tinydream.antit.top/TinydreamWeb/html/dream.html?dream=false'
             })
+            $('.hide').hide();
         }else if(code == 18){
             alert('您当日购买次数已达上限,无法领取该红包');
             window.location.href = 'http://tinydream.antit.top/TinydreamWeb/index.html'
