a96d391ff28fc99b7e9f99895bdbf7278d466b89
whugobarry <17600740311@163.com>
2019-03-01 17:22:45 +0800

    页面优化

--- js/record.js
+++ js/record.js
@@ -41,7 +41,7 @@ $(function(){
             if(state == 'get'){
                 // 收到红包
                 // 数组降序排序
-                arr.sort(compare("gtime"))
+                arr[0].sort(compare("gtime"))
                 $('.tip_txt').html('收到编号<span style="color:#f25542">'+data.stats.countPack+'</span>个').css('font-size','0.3rem');
                 console.log(arr)
                 _.each(arr[0],function(item){
@@ -83,7 +83,7 @@ $(function(){
             }else if(state == 'give'){
                 // $('.content').empty();
                 // 发出红包
-                data.packs.sort(compare("ctime"))
+                arr[0].sort(compare("ctime"))
                 $('.tip_txt').html('发出红包金额<span style="color:#f25542">'+data.stats.totalBill / 100+'</span>元').css('font-size','0.3rem');
                 _.each(arr[0],function(item){
                     // 发出时间
