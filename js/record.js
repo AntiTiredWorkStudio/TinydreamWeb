647006b98c80dbaf61fbc45f80013af8c52d9785
whugobarry <17600740311@163.com>
2019-03-01 16:22:19 +0800

    页面优化

--- js/record.js
+++ js/record.js
@@ -110,10 +110,10 @@ $(function(){
                     },function(code,data){
                         var user = data.selfinfo;
                         console.log(user.nickname)
-                        $('<div class="info"><div class="left"><p class="username">'+user.nickname+'</p><p class="time">'+time+'</p></div><div class="right"><p class="coun">'+item.rcount+'个</p><p class="f_count">'+item.gcount+'/'+item.rcount+'</p></div></div>').appendTo('.content')
                         if(item.pstate == 'FINISHED' && item.gcount < item.rcount){
-                            $('.right .coun').html(item.rcount+'个').css('text-align','right');
-                            $('.right .f_count').html('已过期'+item.gcount+'/'+item.rcount).css('text-align','right')
+                            $('<div class="info"><div class="left"><p class="username">'+user.nickname+'</p><p class="time">'+time+'</p></div><div class="right"><p class="coun">'+item.rcount+'个</p><p class="f_count">已过期'+item.gcount+'/'+item.rcount+'</p></div></div>').css('text-align','center').appendTo('.content')
+                        }else{
+                            $('<div class="info"><div class="left"><p class="username">'+user.nickname+'</p><p class="time">'+time+'</p></div><div class="right"><p class="coun">'+item.rcount+'个</p><p class="f_count">'+item.gcount+'/'+item.rcount+'</p></div></div>').appendTo('.content')
                         }
                     })
                 })
