73c406e88cefe69cae675de87f558ace1984ab1c
whugobarry <17600740311@163.com>
2019-03-01 14:03:31 +0800

    页面优化

--- js/record.js
+++ js/record.js
@@ -12,7 +12,7 @@ $(function(){
         return function(a,b){
             var value1 = a[property];
             var value2 = b[property];
-            return value2 - value1;
+            return value1 - value2;
         }
     }
 
