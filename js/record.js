b8f44f7faa6edf19e9895ca1cba66b4e9e03e313
whugobarry <17600740311@163.com>
2019-03-01 15:56:41 +0800

    页面优化

--- js/record.js
+++ js/record.js
@@ -106,6 +106,7 @@ $(function(){
                         uid:item.uid
                     },function(code,data){
                         var user = data.selfinfo;
+                        console.log(user.nickname)
                         $('<div class="info"><div class="left"><p class="username">'+user.nickname+'</p><p class="time">'+time+'</p></div><div class="right"><p class="coun">'+item.rcount+'个</p><p class="f_count">'+item.gcount+'/'+item.rcount+'</p></div></div>').appendTo('.content')
                         if(item.pstate == 'FINISHED' && item.gcount < item.rcount){
                             $('<div class="info"><div class="left"><p class="username" style="text-align:left">'+item.nickname+'</p><p class="time">'+time+'</p></div><div class="right"><p class="coun" style="text-align:right">'+item.rcount+'个</p><p class="f_count" style="text-align:right">已过期'+item.gcount+'/'+item.rcount+'</p></div></div>').css('text-align','right').appendTo('.content') 
