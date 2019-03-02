2fed8cb09ae95585a66f87db2cea08adce59d700
whugobarry <17600740311@163.com>
2019-03-02 22:54:14 +0800

    项目上线

--- js/getR_Pack.js
+++ js/getR_Pack.js
@@ -2,7 +2,7 @@ $(function(){
 $.showLoading("数据加载中");
 WebApp.Init('wxc5216d15dd321ac5',//appid
         function(result,data){
-			$.showLoading("数据加载中");
+			// $.showLoading("数据加载中");
 			var rid = $_GET.rid;
 			TD_Request('rp','grp',{rid:rid},function(code,data){
 				console.log(data)
@@ -24,7 +24,7 @@ WebApp.Init('wxc5216d15dd321ac5',//appid
 				});
 				$('.title').html(userInfo.nickname+'的梦想红包');
 				$('.msg').html(data.redpack.content);
-				$.hideLoading();
+				// $.hideLoading();
 				$('button').click(function(){
 					localStorage.setItem('rid',rid);
 					localStorage.setItem('rinfo',JSON.stringify({pid:pid,ptbill:ptbill,rid:rid,headicon:userInfo.headicon,nickname:userInfo.nickname,content:data.redpack.content}));
