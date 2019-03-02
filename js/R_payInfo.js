702052aa5e2f5e217154df06cb5cf3b5be425e34
whugobarry <17600740311@163.com>
2019-03-02 22:15:30 +0800

    项目上线

--- js/R_payInfo.js
+++ js/R_payInfo.js
@@ -30,7 +30,10 @@ $(function(){
                 onConfirm: function (result) {
                     console.log(result);
                     $("#input-pid").attr("value",result[0].label);
-                    $('#input-pid').attr('data-ubill',result[0].value)
+                    $('#input-pid').attr('data-ubill',result[0].value);
+                    if($('.right input').val() != ''){
+                        $('#bill').html($('.right input').val() * $('#input-pid').attr('data-ubill'));
+                    }
                 },
                 id:'singleLinePicker'
             })
