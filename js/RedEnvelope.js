254dc0038f2bd4d131de12c1e1d62ed85b6e2621
whugobarry <17600740311@163.com>
2019-03-05 13:50:00 +0800

    支付海报分享

--- js/RedEnvelope.js
+++ js/RedEnvelope.js
@@ -1,29 +1,50 @@
 WebApp.JSAPI.Init();
 $(function(){
-    TD_Request('us','enterpack',{
-        uid:data.openid,
-        nickname:data.nickname,
-        headicon:data.headimgurl
-    },function(code,data){
-
-    },function(code,data){
-        console.log(data)
-    })
-    // 红包记录处理
-    $('.record span').click(function(){
-        window.location.href = 'http://tinydream.antit.top/TinydreamWeb/html/record.html?time='+new Date().getTime()
-    })
-    // 普通红包处理
-    $('.ordinary').click(function(){
-        window.location.href = 'http://tinydream.antit.top/TinydreamWeb/html/R_payInfo.html?time='+new Date().getTime()
-    })
-    // 红包规则处理
-    $('.rules').click(function(){
-        window.location.href = 'http://tinydream.antit.top/TinydreamWeb/html/R_rules.html?time='+new Date().getTime()
-    })
-    // 手气红包处理
-    $('.luck').click(function(){
-        alert('手气红包尚未上线，敬请期待……');
-        return;
-    })
+    if(typeof Options.GetUserInfo() == 'undefined' || Options.GetUserInfo() == '' || Options.GetUserInfo() == null){
+        WebApp.Init('wxc5216d15dd321ac5',function(result,data){
+            TD_Request('us','enterpack',{
+                uid:data.openid,
+                nickname:data.nickname,
+                headicon:data.headimgurl
+            },function(code,data){
+                // 红包记录处理
+                $('.record span').click(function(){
+                    window.location.href = 'http://tinydream.antit.top/TinydreamWeb/html/record.html?time='+new Date().getTime()
+                })
+                // 普通红包处理
+                $('.ordinary').click(function(){
+                    window.location.href = 'http://tinydream.antit.top/TinydreamWeb/html/R_payInfo.html?time='+new Date().getTime()
+                })
+                // 红包规则处理
+                $('.rules').click(function(){
+                    window.location.href = 'http://tinydream.antit.top/TinydreamWeb/html/R_rules.html?time='+new Date().getTime()
+                })
+                // 手气红包处理
+                $('.luck').click(function(){
+                    alert('手气红包尚未上线，敬请期待……');
+                    return;
+                })
+            },function(code,data){
+                console.log(data)
+            })
+        })
+    }else{
+        // 红包记录处理
+        $('.record span').click(function(){
+            window.location.href = 'http://tinydream.antit.top/TinydreamWeb/html/record.html?time='+new Date().getTime()
+        })
+        // 普通红包处理
+        $('.ordinary').click(function(){
+            window.location.href = 'http://tinydream.antit.top/TinydreamWeb/html/R_payInfo.html?time='+new Date().getTime()
+        })
+        // 红包规则处理
+        $('.rules').click(function(){
+            window.location.href = 'http://tinydream.antit.top/TinydreamWeb/html/R_rules.html?time='+new Date().getTime()
+        })
+        // 手气红包处理
+        $('.luck').click(function(){
+            alert('手气红包尚未上线，敬请期待……');
+            return;
+        })
+    }
 })
