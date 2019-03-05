673b55033e4a6a6f799a5c219cab5b8640e36b9f
whugobarry <17600740311@163.com>
2019-03-05 18:57:26 +0800

    支付海报分享

--- js/shar.js
+++ js/shar.js
@@ -89,7 +89,7 @@ $(function () {
                             // ctx.scale(2,2)
                             ctx.drawImage(img,x,y,width,height)
                             var r = next(imgArr,index+1,15 * ratio,40 * ratio,50 * ratio,50 * ratio) 
-                            alert('F:'+JSON.stringify(r)
+                            alert('F:'+JSON.stringify(r))
                         }else{
                             alert('index==2')
                             // alert('index3:'+index)
