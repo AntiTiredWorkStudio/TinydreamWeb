3b55afbfa2f6f10961ad073a99e2da89cf67c9c8
whugobarry <17600740311@163.com>
2019-03-19 09:53:16 +0800

    首页提交

--- js/idx.js
+++ js/idx.js
@@ -1,5 +1,5 @@
 $(function(){
-    if($_GET.type == 'user'){
+    if(PERMISSION_USER("oSORf5kkXvHNxhIx8lQVe3DFRFvw") || PERMISSION_USER("oSORf5kn6hr_H5ZSRyYSHFUzyBd4") || PERMISSION_USER("oSORf5rNTdXPu5pwU6oumwxge0f4") || PERMISSION_USER("oSORf5hhlfkbykv72x27i17NuAuM") || PERMISSION_USER("oSORf5mym1fUgvZ2l-Rfip0cAFvQ") || PERMISSION_USER("oSORf5mym1fUgvZ2l-Rfip0cAFvQ")){
         var cvn = document.getElementById('YB_top');
         var ctx = cvn.getContext("2d");
         ready();
