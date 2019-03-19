8c81fa486ea7b9bb1810f58ff50c2d932d5b94a3
whugobarry <17600740311@163.com>
2019-03-19 11:53:35 +0800

    首页提交

--- js/index.js
+++ js/index.js
@@ -10,36 +10,37 @@ $(function(){
            console.log(result,data);
            var userInfo = Options.GetUserInfo();
            if(PERMISSION_USER(userInfo.openid)){
-            var cvn = document.getElementById('YB_top');
-            var ctx = cvn.getContext("2d");
-            ready();
-            drawCircle(ctx,0.5)
-            function ready(){
-                var canvas = document.getElementById('YB_bottom');
-                var cxt_arc = canvas.getContext("2d");
-                cxt_arc.lineWidth = 15;
-                cxt_arc.strokeStyle = '#edf0f5';
-                cxt_arc.lineCap = 'round';
-                cxt_arc.beginPath();
-                cxt_arc.arc(95, 95, 80, 0, 2 * Math.PI, false);
-                cxt_arc.stroke();
-            }
-            function drawCircle(ctx,prop){
-                if(prop == 0){
-                    ctx.clearRect(0,0,190,190);
-                }else{
-                    prop = prop * 2;
-                }
-                ctx.clearRect(0,0,190,190);
-                ctx.fillStyle = 'white';
-                ctx.clearRect(0,0,190,190);
-                ctx.lineWidth = 15;
-                ctx.strokeStyle = '#ffc057';
-                ctx.lineCap = 'round';
-                ctx.beginPath();
-                ctx.arc(95, 95, 80, Math.PI/-2, prop * Math.PI - Math.PI / 2, false);
-                ctx.stroke()
-            }
+               console.log(userInfo.openid);
+            // var cvn = document.getElementById('YB_top');
+            // var ctx = cvn.getContext("2d");
+            // ready();
+            // drawCircle(ctx,0.5)
+            // function ready(){
+            //     var canvas = document.getElementById('YB_bottom');
+            //     var cxt_arc = canvas.getContext("2d");
+            //     cxt_arc.lineWidth = 15;
+            //     cxt_arc.strokeStyle = '#edf0f5';
+            //     cxt_arc.lineCap = 'round';
+            //     cxt_arc.beginPath();
+            //     cxt_arc.arc(95, 95, 80, 0, 2 * Math.PI, false);
+            //     cxt_arc.stroke();
+            // }
+            // function drawCircle(ctx,prop){
+            //     if(prop == 0){
+            //         ctx.clearRect(0,0,190,190);
+            //     }else{
+            //         prop = prop * 2;
+            //     }
+            //     ctx.clearRect(0,0,190,190);
+            //     ctx.fillStyle = 'white';
+            //     ctx.clearRect(0,0,190,190);
+            //     ctx.lineWidth = 15;
+            //     ctx.strokeStyle = '#ffc057';
+            //     ctx.lineCap = 'round';
+            //     ctx.beginPath();
+            //     ctx.arc(95, 95, 80, Math.PI/-2, prop * Math.PI - Math.PI / 2, false);
+            //     ctx.stroke()
+            // }
         }else{
             $('.YB_help').hide()
         }
