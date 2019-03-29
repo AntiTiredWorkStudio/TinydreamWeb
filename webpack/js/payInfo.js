e54c21e49b60095a1f851695dcfc04bee2e1b2f1
whugobarry <17600740311@163.com>
2019-03-29 13:00:11 +0800

    提交修改

--- webpack/js/payInfo.js
+++ webpack/js/payInfo.js
@@ -153,19 +153,38 @@ var pay = new Vue({
         },
         // 微信支付
         Pay(){
-            this.wxPay(this.oid,this.pay * 100,uid);
+            this.wxPay(this.oid,this.pay * 100,uid,this);
         },
         // 统一下单
-        wxPay(oid,bill,uid){
+        wxPay(oid,bill,uid,vue){
             TD_Request('ds','wxpayweb',{
                 oid:oid,
                 bill:bill,
                 uid:uid
             },function(code,data){
                 console.log(data)
+                vue.WXPAYJS(data.appId,data.timeStamp,data.nonceStr,data.package,data.signType,data.paySign,vue)
             },function(code,data){
                 console.log(data)
             })
+        },
+        // 唤醒支付
+        WXPAYJS(appid,timeStamp,nonceStr,package,signType,paySign,self){
+            WeixinJSBridge.invoke(
+                'getBrandWCPayRequest', {
+                "appId":appid,     //公众号名称，由商户传入     
+                "timeStamp":timeStamp,         //时间戳，自1970年以来的秒数     
+                "nonceStr":nonceStr, //随机串     
+                "package":package,     
+                "signType":signType,         //微信签名方式：     
+                "paySign":paySign//微信签名 
+                },function(res){
+                  if(res.err_msg == "get_brand_wcpay_request:ok" ){
+                  // 使用以上方式判断前端返回,微信团队郑重提示：
+                        //res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
+                        alert(JSON.stringify(res))
+                  } 
+               });
         }
     }
 })
