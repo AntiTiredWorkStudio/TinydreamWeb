70895c0f2a2f0a6946b963dee202afed25cbe660
whugobarry <17600740311@163.com>
2019-03-29 13:01:35 +0800

    提交修改

--- webpack/js/payInfo.js
+++ webpack/js/payInfo.js
@@ -179,6 +179,7 @@ var pay = new Vue({
                 "signType":signType,         //微信签名方式：     
                 "paySign":paySign//微信签名 
                 },function(res){
+                    alert(JSON.stringify(res))
                   if(res.err_msg == "get_brand_wcpay_request:ok" ){
                   // 使用以上方式判断前端返回,微信团队郑重提示：
                         //res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
