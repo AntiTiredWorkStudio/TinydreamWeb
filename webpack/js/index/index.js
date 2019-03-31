c63dca0bcb903cd42aa1903d95214b8691141dc6
whugobarry <17600740311@163.com>
2019-04-01 04:22:10 +0800

    提交

--- webpack/js/index/index.js
+++ webpack/js/index/index.js
@@ -2,6 +2,9 @@ var app = new Vue({
     el:'#app',
     data:{
         userInfo:'',//用户信息
+        mainpool:'',//小梦想互助梦想池
+        maintrade:'',//小生意互助池
+        buyinfo:'',//购买信息
     },
     mounted(){
         this.GetUserInfo(this)
@@ -16,24 +19,38 @@ var app = new Vue({
                     Options.TestServer = true;
                 }
                 // 注册个人信息
-                self.Register(self.userInfo.openid,self.userInfo.nickname,self.userInfo.headimgurl)
-                // $('.screen_main').html('暂无人购买').css({
-                //     color:'#999',
-                //     'font-size':'0.24rem'
-                // })
+                self.Register(self.userInfo.openid,self.userInfo.nickname,self.userInfo.headimgurl,self)
+                
             });
         },
         // 注册信息
-        Register(uid,nickname,headicon){
+        Register(uid,nickname,headicon,self){
             TD_Request('us','enter',{
                 uid:uid,
                 nickname:nickname,
                 headicon:headicon
             },function(code,data){
                 console.log(data)
+                if(data.buyinfo.length == 0){
+                    $('.screen_main').html('暂无人购买').css({
+                        color:'#999',
+                        'font-size':'0.24rem',
+                        'text-align':'center'
+                    })
+                }else{
+                    self.buyinfo = data.buyinfo;
+                    // 公屏信息展示
+                    self.Screen();
+                }
             },function(code,data){
                 console.log(data);
             })
+        },
+        // 公屏信息展示;
+        Screen(){
+            let buyInfo = null;
+            buyInfo = this.buyinfo.unshift();
+            console.log(buyInfo)
         }
     }
 })
