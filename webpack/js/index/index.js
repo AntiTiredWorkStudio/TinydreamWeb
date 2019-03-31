c2a61f975c5452607bd3e2501d94e0aa71176d73
whugobarry <17600740311@163.com>
2019-04-01 01:27:29 +0800

    提交

--- webpack/js/index/index.js
+++ webpack/js/index/index.js
@@ -1,9 +1,12 @@
-$(function(){
-  $('.flipster').flipster({
-    itemContainer: 'ul', // Container for the flippin' items.
-    itemSelector:  'li',
-    style:'coverflow',
-    start:'center',
-    enableKeyboard:false,
-  });
+var app = new Vue({
+    el:'#app',
+    data:{
+        userInfo:'',//用户信息
+    },
+    mounted(){
+        WebApp.Init('wxc5216d15dd321ac5',//appid
+        function(result,data){
+            console.log(Options)
+        });
+   }
 })
