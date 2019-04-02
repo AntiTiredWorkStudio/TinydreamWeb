c10ccbe5c0676af62f54aa97ebe6b4e209d741f8
whugobarry <17600740311@163.com>
2019-04-02 15:27:45 +0800

    文案修改

--- webpack/js/index/index.js
+++ webpack/js/index/index.js
@@ -178,8 +178,13 @@ var app = new Vue({
             var time = h + ":" + m + ":" + s;
             return time;
         },
+        // 跳转梦想池列表页面
         dream(){
-            console.log(1)
+            window.location.href = 'html/dreampool/dreampool.html?time='+new Date().getTime()+'&active=0'
+        },
+        // 参与互助
+        join(state){
+            console.log(state);
         }
     }
 })
