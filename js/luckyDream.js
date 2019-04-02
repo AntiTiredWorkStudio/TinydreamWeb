659b5885bb0acb9ad935143389e721aa301d1ce8
whugobarry <17600740311@163.com>
2019-04-02 11:41:23 +0800

    文案修改

--- js/luckyDream.js
+++ js/luckyDream.js
@@ -3,9 +3,11 @@ $(function(){
     var lucky = JSON.parse(localStorage.getItem('lucky'));
     console.log(lucky)
     if(lucky.pool.ptype == 'TRADE'){
-        $('.lucky_tip').html('恭喜您参与的'+lucky.pool.ptitle+'成为幸运者，幸运编号为'+lucky.lottery.lid+'，本期免费获得项目为：'+lucky.title+'.   我们工作人员会在3个工作日内联系您安排项目对接，请您保持电话畅通。 提示：为更好地给您对接项目，请您务必在7个工作日内完成实名认证。')
+        $('.lucky_tip').html('恭喜您参与的'+lucky.pool.ptitle+'成为幸运者，幸运编号为'+lucky.lottery.lid+'，本期免费获得项目为：'+lucky.title+'.   我们工作人员会在3个工作日内联系您安排项目对接，请您保持电话畅通。')
+        $('.tip_message').html('为更好地给您对接项目，请您务必在7个工作日内完成实名认证。')
         $('.look').hide();
     }else{
+        $('.tip_message').html('为了更好地给您颁发梦想互助金，请您务必在7个工作日内完善梦想和实名认证！')
         $('.lucky_tip').html('恭喜您参与的梦想互助'+lucky.lottery.pid+'期成为幸运者，幸运编号为'+lucky.lottery.lid+'，本期互助资金为'+lucky.pool.cbill / 100+'元')
     }
 	if(lucky.state!="DOING"){
