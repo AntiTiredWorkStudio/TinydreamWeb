b704aaa1a99052a7f487b157b3105c4c30b11d49
whugobarry <17600740311@163.com>
2019-04-02 16:11:24 +0800

    文案修改

--- webpack/js/index/index.js
+++ webpack/js/index/index.js
@@ -200,7 +200,7 @@ var app = new Vue({
                 uid:self.userInfo.openid,
                 pid:pid
             },function(code,data){
-                console.log(data,data.actions.hasOwnProperty('editdram'));
+                console.log(data,data.actions.hasOwnProperty('editdream'));
                 return;
                 if(state == 'trade'){
                     SaveStorage('buy',JSON.stringify(data.actions))
