f3a2e85ec4fc6627aaaa73e72149b85fa7b695ac
whugobarry <17600740311@163.com>
2019-04-05 02:15:19 +0800

    新 ui

--- webpack/js/clockIn/clockIn.js
+++ webpack/js/clockIn/clockIn.js
@@ -4,9 +4,20 @@ var clockIn = new Vue({
     el:'#clockIn',
     data:{
         actives:1,
-        tabbar:''
+        tabbar:'',
+        show:true,//是否显示弹窗
     },
     created(){
         this.tabbar = common.tabbar;
+    },
+    methods:{
+        // 点击购买合约
+        buy(state){
+            if(state == 100){
+                // 100天合约
+            }else if(state == 21){
+                // 21天合约
+            }
+        }
     }
 })
