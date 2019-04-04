9cdec06e10b492b5239951bd5b945d02ff87e6e0
whugobarry <17600740311@163.com>
2019-04-04 19:15:59 +0800

    文案修改

--- webpack/js/user/user.js
+++ webpack/js/user/user.js
@@ -15,8 +15,16 @@ var user = new Vue({
     methods:{
         // 获取个人信息
         GetSelfInfo(self){
+            self.$toast.loading({
+                duration:0,
+                forbidClick:true,
+                loadingType:'circular',
+                message:'获取信息中...'
+            })
             TD_Request('us','selfinfo',{uid:uid},function(code,data){
                 console.log(data);
+                self.$toast.clear();
+                self.selfinfo = data.selfinfo;
             },function(code,data){
                 console.log(data)
             })
