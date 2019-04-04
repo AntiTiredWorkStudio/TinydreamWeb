f223f26e963559e47cfc9a0e5513584f349756b4
whugobarry <17600740311@163.com>
2019-04-04 19:12:44 +0800

    文案修改

--- webpack/js/user/user.js
+++ webpack/js/user/user.js
@@ -4,9 +4,22 @@ var user = new Vue({
     el:'#user',
     data:{
         actives:2,//默认菜单
-        tabbar:''
+        tabbar:'',//tabbar
+        selfinfo:'',//个人信息
     },
     created(){
         this.tabbar = common.tabbar;
+        // 获取个人信息
+        this.GetSelfInfo(self)
+    },
+    methods:{
+        // 获取个人信息
+        GetSelfInfo(self){
+            TD_Request('us','selfinfo',{uid:uid},function(code,data){
+                console.log(data);
+            },function(code,data){
+                console.log(data)
+            })
+        }
     }
 })
