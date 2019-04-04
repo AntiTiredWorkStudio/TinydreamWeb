385544021dafba9fe1d3d73aaae67f756f0302e9
whugobarry <17600740311@163.com>
2019-04-04 19:18:43 +0800

    文案修改

--- webpack/js/user/user.js
+++ webpack/js/user/user.js
@@ -5,7 +5,8 @@ var user = new Vue({
     data:{
         actives:2,//默认菜单
         tabbar:'',//tabbar
-        selfinfo:'',//个人信息
+        selfinfo:'',//个人信息,
+        headicon:'',//个人头像
     },
     created(){
         this.tabbar = common.tabbar;
@@ -24,6 +25,9 @@ var user = new Vue({
             TD_Request('us','selfinfo',{uid:uid},function(code,data){
                 console.log(data);
                 self.$toast.clear();
+                self.headicon = {
+                    background:'url('+data.selfinfo.headicon+') no-repeat center center / 1.32rem 1.32rem'
+                }
                 self.selfinfo = data.selfinfo;
             },function(code,data){
                 console.log(data)
