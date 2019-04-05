8f49486b239eb1e949cd74a400af7a7e30ff417e
whugobarry <17600740311@163.com>
2019-04-05 13:48:46 +0800

    新 ui

--- webpack/js/actionClock/actionClock.js
+++ webpack/js/actionClock/actionClock.js
@@ -5,7 +5,10 @@ var actionClock = new Vue({
     data:{
         actives:1,
         tabbar:'',
-        progress1:0.5,
+        progress1:0.5,//打卡进度
+        day:'50',//距离目标天数
+        continday:'50',//连续打卡天数
+        clockday:'50',//已打卡天数
     },
     created(){
         this.tabbar = common.tabbar;
