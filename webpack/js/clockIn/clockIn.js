27beaf1d03d516dee31300b5b8f331820b763cfe
whugobarry <17600740311@163.com>
2019-04-05 12:51:43 +0800

    新 ui

--- webpack/js/clockIn/clockIn.js
+++ webpack/js/clockIn/clockIn.js
@@ -65,58 +65,58 @@ var clockIn = new Vue({
     created(){
         this.tabbar = common.tabbar;
     },
-    methods:{
-        // 点击购买合约
-        buy(state){
-            if(state == 100){
-                // 100天合约
-                this.day = 100;
-                this.cbill = 99;
-                this.bill = 200;
-                this.show = true;
-            }else if(state == 21){
-                // 21天合约
-                this.day = 21;
-                this.cbill = 98;
-                this.bill = 98;
-                this.show = true;
-            }
-        },
-        // checkbox
-        conType(title,index){
-            console.log(title,index);
-            this.contractType[index].check = !this.contractType[index].check;
-            if(this.contractType[index].check){
-                this.checkArr.push(title)
-            }else{
-                this.removeArray(this.checkArr,title);
-            }
-        },
-        // 数组删除方法
-        removeArray(arr, val) {
-            for(var i = 0; i < arr.length; i++) {
-                if(arr[i] == val) {
-                    arr.splice(i, 1);
-                    break;
-                }
-            }
-        },
-        // 关闭弹窗
-        close(){
-            this.show = false;
-        },
-        // 支付
-        pay(bill){
-            if(this.checkArr == ''){
-                this.$dialog.alert({
-                    title:'温馨提示',
-                    message:'亲~目标才是坚持行动的动力，选择一个行动主题作为坚持目标吧！'
-                }).then(()=>{
-                    return;
-                })
-            }else{
-                console.log('微信支付')
-            }
-        }
-    },
+    // methods:{
+    //     // 点击购买合约
+    //     buy(state){
+    //         if(state == 100){
+    //             // 100天合约
+    //             this.day = 100;
+    //             this.cbill = 99;
+    //             this.bill = 200;
+    //             this.show = true;
+    //         }else if(state == 21){
+    //             // 21天合约
+    //             this.day = 21;
+    //             this.cbill = 98;
+    //             this.bill = 98;
+    //             this.show = true;
+    //         }
+    //     },
+    //     // checkbox
+    //     conType(title,index){
+    //         console.log(title,index);
+    //         this.contractType[index].check = !this.contractType[index].check;
+    //         if(this.contractType[index].check){
+    //             this.checkArr.push(title)
+    //         }else{
+    //             this.removeArray(this.checkArr,title);
+    //         }
+    //     },
+    //     // 数组删除方法
+    //     removeArray(arr, val) {
+    //         for(var i = 0; i < arr.length; i++) {
+    //             if(arr[i] == val) {
+    //                 arr.splice(i, 1);
+    //                 break;
+    //             }
+    //         }
+    //     },
+    //     // 关闭弹窗
+    //     close(){
+    //         this.show = false;
+    //     },
+    //     // 支付
+    //     pay(bill){
+    //         if(this.checkArr == ''){
+    //             this.$dialog.alert({
+    //                 title:'温馨提示',
+    //                 message:'亲~目标才是坚持行动的动力，选择一个行动主题作为坚持目标吧！'
+    //             }).then(()=>{
+    //                 return;
+    //             })
+    //         }else{
+    //             console.log('微信支付')
+    //         }
+    //     }
+    // },
 })
