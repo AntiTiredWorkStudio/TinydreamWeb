b235cc1f6399cc956c7f70eed2f05e5423a13c63
whugobarry <17600740311@163.com>
2019-04-10 09:56:38 +0800

    增加支付逻辑

--- webpack/js/clockIn/clockIn.js
+++ webpack/js/clockIn/clockIn.js
@@ -66,6 +66,8 @@ var clockIn = new Vue({
                 })
             }else{
                 console.log(bill,cid)
+                let theme = this.checkArr.join(',');
+                console.log(theme);
                 this.wxpay(this,cid,bill)
             }
         },
@@ -108,11 +110,46 @@ var clockIn = new Vue({
         },
         // 微信支付
         wxpay(self,cid,bill){
+            self.$toast.loading({
+                duraction:0,
+                forbidClick:true,
+                loadingType:'circular',
+                message:'准备中...'
+            })
+            
             TD_Request('op','joi',{cid:cid,uid:uid},function(code,data){
                 console.log(data)
+                self.$toast.clear();
+                self.wxpayweb(self,pay,data.order.oid,cid)
             },function(code,data){
+                self.$toast.clear();
                 console.log(data);
             })
+        },
+        // 唤醒支付
+        wxpayweb(self,pay,oid,cid){
+            self.$toast.loading({
+                duraction:0,
+                forbidClick:true,
+                loadingType:'circular',
+                message:'支付中...'
+            })
+            WeixinJSBridge.invoke(
+                'getBrandWCPayRequest', {
+                "appId":pay.appId,     //公众号名称，由商户传入     
+                "timeStamp":pay.timeStamp,         //时间戳，自1970年以来的秒数     
+                "nonceStr":pay.nonceStr, //随机串     
+                "package":pay.package,     
+                "signType":pay.signType,         //微信签名方式：     
+                "paySign":pay.paySign //微信签名 
+                },function(res){
+                  if(res.err_msg == "get_brand_wcpay_request:ok" ){
+                  // 使用以上方式判断前端返回,微信团队郑重提示：
+                        //res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
+                        self.$toast.success('支付成功');
+                        self.paySuccess(cid,oid,uid,)
+                  } 
+               }); 
         }
     },
 })
