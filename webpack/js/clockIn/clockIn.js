474d7c2d55b40cfcb4a22712bcff280569f114f8
whugobarry <17600740311@163.com>
2019-04-10 20:08:05 +0800

    增加支付逻辑

--- webpack/js/clockIn/clockIn.js
+++ webpack/js/clockIn/clockIn.js
@@ -233,7 +233,7 @@ var clockIn = new Vue({
     watch:{
         value(data){
             if(data.length >= 20){
-                this.value = data.substr(0);
+                this.value = data.substr(0,20);
                 console.log(data,this.value)
             }
         }
