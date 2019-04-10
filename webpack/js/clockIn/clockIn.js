5dae7f979c36720374724649aed21670c346bcf2
whugobarry <17600740311@163.com>
2019-04-10 08:30:10 +0800

    增加支付逻辑

--- webpack/js/clockIn/clockIn.js
+++ webpack/js/clockIn/clockIn.js
@@ -82,7 +82,7 @@ var clockIn = new Vue({
                 self.contractType = data.themes;
                 self.contracts = data.contracts;
                 $.each(self.contracts,function(index,item){
-                    self.contracts[index].description = self.contracts[index].description.substr(0,15) + '<br />'+ self.contracts[index].description.substr(16)
+                    self.contracts[index].description = self.contracts[index].description.substr(0,15) + '<br>'+ self.contracts[index].description.substr(15)
                 })
             },function(code,data){
                 alert(data.context)
