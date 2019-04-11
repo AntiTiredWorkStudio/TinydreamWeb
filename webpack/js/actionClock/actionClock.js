a0f9395b758df460c2c338ca434fe6e7663f5e60
whugobarry <17600740311@163.com>
2019-04-11 12:43:14 +0800

    增加支付逻辑

--- webpack/js/actionClock/actionClock.js
+++ webpack/js/actionClock/actionClock.js
@@ -8,7 +8,10 @@ var actionClock = new Vue({
         progress1:0.5,//打卡进度
         day:'50',//距离目标天数
         continday:'50',//连续打卡天数
-        clockday:'50',//已打卡天数
+        clockday:'50',//已打卡天数,
+        week:["日","一","二","三","四","五","六"],//周期
+        days:'',//日期
+        currentMonth:'',//当前月份
     },
     created(){
         this.tabbar = common.tabbar;
@@ -23,7 +26,9 @@ var actionClock = new Vue({
         Mat(self){
             TD_Request('op','cal',{uid:uid,seek:0},function(code,data){
                 console.log(data)
-                console.log(data.calendar.days)
+                console.log(data.calendar.days);
+                self.days = data.calendar.days;
+                self.currentMonth = data.calendar.currentMonth.substr(0,4) + '.' + data.calendar.currentMonth.substr(4,6);
             },function(code,data){
                 console.log(data);
             })
