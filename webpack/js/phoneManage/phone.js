837c78351662fb7a32629349d3f854d82639ede0
whugobarry <17600740311@163.com>
2019-04-12 18:53:15 +0800

    手机号管理

--- webpack/js/phoneManage/phone.js
+++ webpack/js/phoneManage/phone.js
@@ -4,7 +4,8 @@ var uid = Options.GetUserInfo().openid;
 var phone = new Vue({
     el:'#phone',
     data:{
-        title:'请输入需要绑定的手机号'
+        title:'请输入需要绑定的手机号',
+        tip:'当前手机号为1777777777'
     },
     methods:{
 
