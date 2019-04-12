b3804a1d97038d4a4eb3baad67b8836d53c32f71
whugobarry <17600740311@163.com>
2019-04-12 15:04:06 +0800

    行动打卡页面

--- webpack/js/actionClock/actionClock.js
+++ webpack/js/actionClock/actionClock.js
@@ -10,7 +10,14 @@ var actionClock = new Vue({
         continday:'50',//连续打卡天数
         clockday:'50',//已打卡天数,
         week:["日","一","二","三","四","五","六"],//周期
-        days:'',//日期
+        day_1:'',//星期一的数组
+        day_2:'',//星期二的数组
+        day_3:'',//星期三的数组
+        day_4:'',//星期四的数组
+        day_5:'',//星期五的数组
+        day_6:'',//星期六的数组
+        day_7:'',//星期日的数组
+        days:'',
         currentMonth:'',//当前月份
     },
     created(){
@@ -28,7 +35,8 @@ var actionClock = new Vue({
             TD_Request('op','cal',{uid:uid,seek:0},function(code,data){
                 console.log(data)
                 console.log(data.calendar.days);
-                self.days = data.calendar.days;
+                self.day_1 = data.calendar.days.filter(v=>v.weekDay == '星期一');
+                console.log(day_1)
                 self.currentMonth = data.calendar.currentMonth.substr(0,4) + '.' + data.calendar.currentMonth.substr(4,6);
             },function(code,data){
                 console.log(data);
