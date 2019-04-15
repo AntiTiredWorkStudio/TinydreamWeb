0b914370c3b0cbdc490efee9e41927809aee2b68
whugobarry <17600740311@163.com>
2019-04-16 01:17:12 +0800

    打卡管理

--- webpack/js/fill/fill.js
+++ webpack/js/fill/fill.js
@@ -20,5 +20,24 @@ var fill = new Vue({
                 hedicon:'http://thirdwx.qlogo.cn/mmopen/vi_32/PiajxSqBRaELNMOWpmBVxdBriaDWJbibLPK6vLTg6NsL9QYM4AlDWibl1EcE8zEibzqCvaw1YksQOuGUgnTWwWTsa0Q/132'
             },
         ],//头像
+    },
+    created(){
+        this.info(this)
+    },
+    methods:{
+        // 详情信息
+        info(self){
+            self.$toast.loading({
+                duration:0,
+                forbidClick:true,
+                loadingType:'circular',
+                message:'信息加载中...'
+            })
+            TD_Request('op','uinfo',{uid:uid},function(code,data){
+                console.log(data)
+            },function(code,data){
+                console.log(data);
+            })
+        }
     }
 })
