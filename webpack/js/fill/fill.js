497951244fb2ce0c9fec769b8c5f6517348edfb2
whugobarry <17600740311@163.com>
2019-04-16 01:04:47 +0800

    打卡管理

--- webpack/js/fill/fill.js
+++ webpack/js/fill/fill.js
@@ -3,6 +3,22 @@ var uid = Options.GetUserInfo().openid;
 var fill = new Vue({
     el:'#fill',
     data:{
-        headicons:'http://thirdwx.qlogo.cn/mmopen/vi_32/PiajxSqBRaELNMOWpmBVxdBriaDWJbibLPK6vLTg6NsL9QYM4AlDWibl1EcE8zEibzqCvaw1YksQOuGUgnTWwWTsa0Q/132',//头像
+        headicons:[
+            {
+                hedicon:'http://thirdwx.qlogo.cn/mmopen/vi_32/PiajxSqBRaELNMOWpmBVxdBriaDWJbibLPK6vLTg6NsL9QYM4AlDWibl1EcE8zEibzqCvaw1YksQOuGUgnTWwWTsa0Q/132'
+            },
+            {
+                hedicon:'http://thirdwx.qlogo.cn/mmopen/vi_32/PiajxSqBRaELNMOWpmBVxdBriaDWJbibLPK6vLTg6NsL9QYM4AlDWibl1EcE8zEibzqCvaw1YksQOuGUgnTWwWTsa0Q/132'
+            },
+            {
+                hedicon:'http://thirdwx.qlogo.cn/mmopen/vi_32/PiajxSqBRaELNMOWpmBVxdBriaDWJbibLPK6vLTg6NsL9QYM4AlDWibl1EcE8zEibzqCvaw1YksQOuGUgnTWwWTsa0Q/132'
+            },
+            {
+                hedicon:'http://thirdwx.qlogo.cn/mmopen/vi_32/PiajxSqBRaELNMOWpmBVxdBriaDWJbibLPK6vLTg6NsL9QYM4AlDWibl1EcE8zEibzqCvaw1YksQOuGUgnTWwWTsa0Q/132'
+            },
+            {
+                hedicon:'http://thirdwx.qlogo.cn/mmopen/vi_32/PiajxSqBRaELNMOWpmBVxdBriaDWJbibLPK6vLTg6NsL9QYM4AlDWibl1EcE8zEibzqCvaw1YksQOuGUgnTWwWTsa0Q/132'
+            },
+        ],//头像
     }
 })
