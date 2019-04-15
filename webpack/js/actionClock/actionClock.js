4b0a037bc51083267130def49feeee4f21c40bee
whugobarry <17600740311@163.com>
2019-04-15 11:00:08 +0800

    打卡管理

--- webpack/js/actionClock/actionClock.js
+++ webpack/js/actionClock/actionClock.js
@@ -10,32 +10,26 @@ var actionClock = new Vue({
         continday:'50',//连续打卡天数
         clockday:'50',//已打卡天数,
         week:["日","一","二","三","四","五","六"],//周期
-        day_1:'',//星期一的数组
-        day_2:'',//星期二的数组
-        day_3:'',//星期三的数组
-        day_4:'',//星期四的数组
-        day_5:'',//星期五的数组
-        day_6:'',//星期六的数组
-        day_7:'',//星期日的数组
         days:'',
+        opid:'',//行动 id
         currentMonth:'',//当前月份
     },
     created(){
         this.tabbar = common.tabbar;
         common.notice(this,uid);
-        // 创建行动实例
+        // 创建日历
         this.Mat(this);
     },
     methods:{
         // 打卡
         clock(){
-            console.log('已打卡')
+            this.Clock(this,this.opid);
         },
         Mat(self){
             TD_Request('op','cal',{uid:uid,seek:0,full:'month'},function(code,data){
                 console.log(data)
                 // 创建打卡记录
-                self.Clock(self,data.calendar.opid);
+                self.opid = ata.calendar.opid;
                 console.log(data.calendar.days);
                 self.currentMonth = data.calendar.currentMonth.substr(0,4) + '.' + data.calendar.currentMonth.substr(4,6);
                 self.refreshDate(data.calendar.days)
@@ -72,7 +66,7 @@ var actionClock = new Vue({
                 firstDay = 0;
             }
             for (let i = 0; i < firstDay; i++) {
-                $('<li>&nbsp</li>').appendTo('.weekDate .day');
+                $('<li>&nbsp;</li>').appendTo('.weekDate .day');
             }
             console.log(totalDay,firstDay);
             $.each(days,function(index,item){
