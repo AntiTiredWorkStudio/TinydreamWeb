ee9a7fa68e6527b2b27307d4496b088e57b17a78
whugobarry <17600740311@163.com>
2019-04-16 01:01:57 +0800

    打卡管理

--- webpack/js/fill/fill.js
+++ webpack/js/fill/fill.js
@@ -3,6 +3,6 @@ var uid = Options.GetUserInfo().openid;
 var fill = new Vue({
     el:'#fill',
     data:{
-        
+        headicons:'http://thirdwx.qlogo.cn/mmopen/vi_32/PiajxSqBRaELNMOWpmBVxdBriaDWJbibLPK6vLTg6NsL9QYM4AlDWibl1EcE8zEibzqCvaw1YksQOuGUgnTWwWTsa0Q/132',//头像
     }
 })
