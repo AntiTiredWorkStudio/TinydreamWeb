a92e206c5dd7a5b64a0e9128dfd4a56dafa233e1
whugobarry <17600740311@163.com>
2019-04-16 13:05:01 +0800

    朋友转发进入页面

--- webpack/js/friend/friend.js
+++ webpack/js/friend/friend.js
@@ -3,11 +3,32 @@ var uid = Options.GetUserInfo().openid;
 var friend = new Vue({
     el:'#friend',
     data:{
-
+        contracts:'',//合约列表
+    },
+    created(){
+        // 合约列表
+        this.list(this);
     },
     methods:{
         buy(){
-            
-        }
+
+        },
+        // 合约列表
+        list(self){
+            self.$toast.loading({
+                duraction:0,
+                forbidClick:true,
+                loadingType:'circular',
+                message:'列表获取中...'
+            })
+            TD_Request('co','list',{},function(code,data){
+                self.contractType = data.themes;
+                self.contracts = data.contracts;
+                self.$toast.clear()
+            },function(code,data){
+                self.$toast.clear();
+                alert(data.context)
+            })
+        },
     }
 })
