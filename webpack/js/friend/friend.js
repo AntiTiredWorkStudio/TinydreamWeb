215e56bff2979d67aa328bc5088d8e9a1af385df
whugobarry <17600740311@163.com>
2019-04-16 13:28:41 +0800

    朋友转发进入页面

--- webpack/js/friend/friend.js
+++ webpack/js/friend/friend.js
@@ -5,6 +5,20 @@ var friend = new Vue({
     data:{
         contracts:'',//合约列表
         show:false,//是否显示弹窗
+        cid:'',//合约 id
+        desc:'',//合约描述
+        title:'',//合约标题
+        contractType:'',//可选类型
+        // checkArr:[],//选择的数组
+        price:'',//合约金
+        // bill:'',//返现
+        day:'',//合约天数
+        warm:'',//注意事项
+        status:null,
+        theme:'',//所选主题
+        isshow:false,//是否显示自定义主题
+        custom:'',//自定义主题
+        value:'',//自定义主题
     },
     created(){
         // 合约列表
