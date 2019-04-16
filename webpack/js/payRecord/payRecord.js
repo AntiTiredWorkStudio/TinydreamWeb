4178ebcf662e290b73f4465ba658007301b5e018
whugobarry <17600740311@163.com>
2019-04-16 16:05:11 +0800

    收支记录

--- webpack/js/payRecord/payRecord.js
+++ webpack/js/payRecord/payRecord.js
@@ -4,5 +4,12 @@ var pay = new Vue({
     el:'#payrecord',
     data:{
         active:0,//默认菜单
+        loading:false,//是否处于加载列表状态
+        finished:false,//是否加载完毕
+    },
+    methods:{
+        load(){
+            
+        }
     }
 })
