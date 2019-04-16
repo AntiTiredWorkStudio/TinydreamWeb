18cd779ec4d4860289a77ef17cdd1048d32d344a
whugobarry <17600740311@163.com>
2019-04-16 11:03:47 +0800

    打卡页面

--- webpack/js/fill/fill.js
+++ webpack/js/fill/fill.js
@@ -23,6 +23,7 @@ var fill = new Vue({
                 loadingType:'circular',
                 message:'信息加载中...'
             })
+            var info;
             TD_Request('op','uinfo',{uid:uid},function(code,data){
                 self.$toast.clear();
                 console.log(data)
