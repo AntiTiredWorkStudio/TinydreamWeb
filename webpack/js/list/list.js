a473261711abb47fbe70ff78e0a6c30164926717
whugobarry <17600740311@163.com>
2019-04-17 09:57:37 +0800

    合约列表

--- webpack/js/list/list.js
+++ webpack/js/list/list.js
@@ -17,6 +17,27 @@ var list = new Vue({
         getList(self){
             TD_Request('op','olist',{uid:uid,seek:self.seek,count:10},function(code,data){
                 console.log(data)
+                $.each(data.operations,function(index,item){
+                    if(item.cid == "CO0000000002"){
+                        $('.mt-20').css('background','url(https://tdream.antit.top/hylb100.png) no-repeat center center / 6.8rem 2.7rem')
+                        if(item.state == 'DOING'){
+                            $('.state').css('background','url(https://tdream.antit.top/active.png) no-repeat center center / 1.2rem 1.2rem')
+                        }else if(item.state == 'SUCCESS'){
+                            $('.state').css('background','url(https://tdream.antit.top/actived.png) no-repeat center center / 1.2rem 1.2rem')
+                        }else if(item.state == 'FAIL'){
+                            $('.state').css('background','url(https://tdream.antit.top/unactive.png) no-repeat center center / 1.2rem 1.2rem')
+                        }
+                    }else if(item.cid == "CO0000000001"){
+                        $('.mt-20').css('background','url(https://tdream.antit.top/hylb21.png) no-repeat center center / 6.8rem 2.7rem')
+                        if(item.state == 'DOING'){
+                            $('.state').css('background','url(https://tdream.antit.top/active.png) no-repeat center center / 1.2rem 1.2rem')
+                        }else if(item.state == 'SUCCESS'){
+                            $('.state').css('background','url(https://tdream.antit.top/actived.png) no-repeat center center / 1.2rem 1.2rem')
+                        }else if(item.state == 'FAIL'){
+                            $('.state').css('background','url(https://tdream.antit.top/unactive.png) no-repeat center center / 1.2rem 1.2rem')
+                        }
+                    }
+                })
             },function(code,data){
                 console.log(data)
             })
