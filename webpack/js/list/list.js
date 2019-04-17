5ad94adf08d11cd4e898dd71b8f7e6049a95b03b
whugobarry <17600740311@163.com>
2019-04-17 08:46:01 +0800

    合约列表

---
+++ webpack/js/list/list.js
@@ -0,0 +1,15 @@
+var uid = Options.GetUserInfo().openid;
+
+var list = new Vue({
+    el:"#list",
+    data:{
+        loading:false,
+        finished:false,
+        seek:0
+    },
+    methods:{
+        load(){
+
+        }
+    }
+})
