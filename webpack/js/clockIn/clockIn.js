93a11a89c866a82a3a96b9ed333542afc4ea7a3c
whugobarry <17600740311@163.com>
2019-04-17 23:15:32 +0800

    修改合约列表

--- webpack/js/clockIn/clockIn.js
+++ webpack/js/clockIn/clockIn.js
@@ -264,7 +264,9 @@ var clockIn = new Vue({
                 console.log(data)
                 self.feedback = data.feedback;
                 var order = data.orders.shift();
-                self
+                $.each(data.feedback,function(index,item){
+                    $('.head').css('background','url('+item.headicon+') no-repeat center center / 1rem 1rem');
+                })
                 self.order = order;
                 data.orders.push(order);
                 self.orders = data.orders;
