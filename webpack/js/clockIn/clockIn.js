38c4eb343c01fe1d2fcc882b81d0b6e8fb948f3e
whugobarry <45985481+whugobarry@users.noreply.github.com>
2019-04-19 22:04:31 +0800

    Update clockIn.js
    关闭测试服

--- webpack/js/clockIn/clockIn.js
+++ webpack/js/clockIn/clockIn.js
@@ -37,7 +37,7 @@ var clockIn = new Vue({
     created(){
         this.tabbar = common.tabbar;
         common.notice(this,uid);
-        Options.TestServer = true;
+        Options.TestServer = false;
         // 获取合约列表
         this.list(this)
         this.Orders(this)
