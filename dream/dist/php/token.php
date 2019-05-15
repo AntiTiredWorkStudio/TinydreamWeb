192f87567c5cb01bfafad5c11b9126a85621fbc9
whugobarry <17600740311@163.com>
2019-05-15 14:18:46 +0800

    添加实名认证组件

--- dream/dist/php/token.php
+++ dream/dist/php/token.php
@@ -26,7 +26,7 @@
         public function ID(){
             $url = 'https://aip.baidubce.com/rest/2.0/ocr/v1/idcard?access_token='.$this->getAccessToken();
             // $img = $_POST['path'];
-            var_dump($_FILES['file']);
+            // var_dump($_FILES['file']);
             $file = $_FILES['file']['tmp_name'];
             $name = explode('.',$_FILES['file']['name']);
             $newname =  $this->appid.'.'.$name[1];
