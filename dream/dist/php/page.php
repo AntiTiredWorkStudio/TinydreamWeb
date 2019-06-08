fe8be2f8593f5f0e4a5a9460c17178574ca7a5b3
whugobarry <17600740311@163.com>
2019-06-08 10:55:38 +0800

    files

--- dream/dist/php/page.php
+++ dream/dist/php/page.php
@@ -1,7 +1,7 @@
 <?php 
     $filename = __DIR__.'/cache/page.php';
     $page = $_REQUEST['page'];
-    $file = file_put_contents($filename,'<?php echo '.$page.'?>');
+    $file = file_put_contents($filename,'<?php echo "'.$page.'"?>');
     if($file){
         echo '1';
     }else{
