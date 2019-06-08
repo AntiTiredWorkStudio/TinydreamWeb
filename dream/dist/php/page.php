83cee51e329aae29d359490fc32f1f7ccb7720af
whugobarry <17600740311@163.com>
2019-06-08 10:45:22 +0800

    files

--- dream/dist/php/page.php
+++ dream/dist/php/page.php
@@ -2,7 +2,7 @@
     $filename = __DIR__.'/cache/page.php';
     $page = $_REQUEST['page'];
     $arr = array('page'=>$page);
-    $file = file_get_contents($filename,'<?php return '.var_export($arr,true).' ?>');
+    $file = file_put_contents($filename,'<?php return '.var_export($arr,true).' ?>');
     if($file){
         echo '1';
     }else{
